879f4e6df84f6747c3337e243d184bcf35747079
Kou <hikkappi@yahoo.co.jp>
2022-10-20 06:56:14 +0900

    講師、講座テーブルのマイグレーションファイルを作成。

---
+++ database/migrations/2022_10_19_215030_create_instructors_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInstructorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('instructors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50)->comment('講師名前');
+            $table->string('email', 255)->comment('メールアドレス');
+            $table->string('password', 255)->comment('パスワード');
+            $table->dateTime('created_at');
+            $table->dateTime('update_at');
+            $table->dateTime('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('instructors');
+    }
+}

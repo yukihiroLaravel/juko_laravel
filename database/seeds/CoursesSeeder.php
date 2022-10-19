a594f0583472eec87fe9a2f71b478b0788cf379f
Kou <hikkappi@yahoo.co.jp>
2022-10-20 07:28:45 +0900

    講師、講座のシーダーを作成。

---
+++ database/seeds/CoursesSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Model\Course;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+
+class CoursesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Course::insert([
+            [
+                'instructor_id' => 1,
+                'title' => 'PHP入門講座',
+                'image' => 'course/1/thumbnail.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ],
+            [
+                'instructor_id' => 1,
+                'title' => 'Laravel入門講座',
+                'image' => 'course/2/thumbnail.png',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
+            ]
+        ]);
+    }
+}

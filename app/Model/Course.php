0cddf92ad17463c0392475006fd859d856ec660a
Kou <hikkappi@yahoo.co.jp>
2022-10-20 07:28:30 +0900

    講師、講座モデルを作成。

---
+++ app/Model/Course.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    /**
+     * モデルと関連しているテーブル
+     *
+     * @var string
+     */
+    protected $table = 'courses';
+
+    /**
+     * 講師を取得
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function instructor()
+    {
+        return $this->belongsTo(Instructor::class);
+    }
+}

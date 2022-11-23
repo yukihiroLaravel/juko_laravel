367aacf494cc30b499b2d8bfccbd968ca90a4204
nakamichi yuta <prnkmt2947@gmail.com>
2022-11-23 09:44:39 +0900

    lessonApi実装ゆうた

--- app/Http/Controllers/Api/LessonController.php
+++ app/Http/Controllers/Api/LessonController.php
@@ -5,34 +5,34 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LessonGetRequest;
 use App\Http\Resources\LessonGetResponse;
-use App\Model\Attendance;
+use App\Model\Attendane;
 
 
 class LessonController extends Controller
 {
     public function index(LessonGetRequest $request)
     {
-      $attendance = Attendance::with(['course.chapter.lesson', 'student', 'lessonAttendance'])
-      ->where('id', $request->attendance_id)
-      ->first();
+      $attendance = Attendance::with(['course.chapter.lesson', 'student', 'lessonAttendanes'])
+      ->where('id', $request->attendance_id)->first();
+        $result = [
+          'chapter_id' => $attendance->id,
+          'title' => $attendance->title,
+          'lessons' => $attendance->lesson
+        ];
+        return response()->json($result);
+        /* foreach($attendance->lesson as $lesson){
+          $result = [
+            'chapter_id' => $lesson->chapter_id,
+            'title' => $lesson->chapter->title,
+              'lesson' => [
+                'lesson_id' => $lesson->lessonAttendance->lesson_id
+                
+              ],
+          ];
+          return response()->json($result);
 
-      /* return response()->json($lesson);*/
-       /* $result = [
-        'title' => $lesson->title,
-        'lessons' => []
-      ];  */
-      
-       foreach($attendance->course->chapter as $chapter){
-         if($chapter->id == $request->chapter_id){
-             return response()->json($chapter);
-         }
-        /* $result['lesson'][] = [
-        'chapter_id' => $chapter->id,
-        'title' => $chapter->title,
-        ]; */
-       
-      }
-      
-  }
-      /* return new LessonGetResponse($lesson); */
+          } */
+          
+    }
+        
 }

2a343e5fe859f128e4a1d9080a35d4378e740f8d
daisuke takebuchi <bucchi_orange@yahoo.co.jp>
2023-01-21 23:49:37 +0900

    ステータスの更新

--- app/Http/Controllers/Api/LessonAttendanceController.php
+++ app/Http/Controllers/Api/LessonAttendanceController.php
@@ -10,9 +10,18 @@ class LessonAttendanceController extends Controller
 {
     public function update(Request $request)
     {
+        //　ToDo　ログインユーザーとレッスン受講状態の受講者が一致するか検証が必要
+        LessonAttendance::where('id','=',$request->lesson_attendance_id) //whereは条件付け
+        ->update([
+            'status' => $request->status,
+        ]);
+
     return response()->json([
-        "lesson_attendance_id" => $request->lesson_attendance_id,
-        "lesson_attendance_id" => LessonAttendance::find($request)
+       //"lesson_attendance_id" => $request->lesson_attendance_id,
+        
+       "lesson_attendance_id" => LessonAttendance::where('id','=',$request->lesson_attendance_id)->first()
     ]);
     }
+
+
 }

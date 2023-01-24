d1c260d675567a1dd16b57cbde8a66f70a197f22
daisuke takebuchi <bucchi_orange@yahoo.co.jp>
2023-01-24 23:18:21 +0900

    ステータスの更新とレスポンスにresult:trueの表示

--- app/Http/Controllers/Api/LessonAttendanceController.php
+++ app/Http/Controllers/Api/LessonAttendanceController.php
@@ -11,16 +11,14 @@ class LessonAttendanceController extends Controller
     public function update(Request $request)
     {
         //　ToDo　ログインユーザーとレッスン受講状態の受講者が一致するか検証が必要
-        LessonAttendance::where('id','=',$request->lesson_attendance_id) //whereは条件付け
+        LessonAttendance::where('id','=',$request->lesson_attendance_id)
         ->update([
             'status' => $request->status,
         ]);
 
-    return response()->json([
-       //"lesson_attendance_id" => $request->lesson_attendance_id,
-        
-       "lesson_attendance_id" => LessonAttendance::where('id','=',$request->lesson_attendance_id)->first()
-    ]);
+        return response()->json([
+            "result" => true,
+        ]);
     }
 
 

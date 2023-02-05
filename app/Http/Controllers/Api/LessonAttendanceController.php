c836985a44729472290c722e6789d8cb1cfcc60f
daisuke takebuchi <bucchi_orange@yahoo.co.jp>
2023-02-05 09:12:56 +0900

    仮ログインユーザーとレッスンidの一致検証。エラーログ

--- app/Http/Controllers/Api/LessonAttendanceController.php
+++ app/Http/Controllers/Api/LessonAttendanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LessonAttendancePatchRequest;
 use App\Model\LessonAttendance;
+use Illuminate\Support\Facades\Log;
 
 class LessonAttendanceController extends Controller
 {
@@ -12,15 +13,26 @@ class LessonAttendanceController extends Controller
     {
         // ToDo 認証ユーザーを一時的にid=1とする。
         $authId = 1;
-        try {
+        try { //throw new \Exception("aa");
 
-            //　ToDo　ログインユーザーとレッスン受講者idが一致するか検証が必要
+            //　ToDo　ログインユーザーとレッスン受講者idが一致するか検証
             $lessonAttendance = LessonAttendance::where('id', '=', $request->lesson_attendance_id)->first();
             if ($authId !== $lessonAttendance->attendance->student_id) {
                 return response()->json([
-                    "result" => false
+                    "result" => false,
+                    "error_code" => 403
+                ]);
+            } else if ($authId === null) {
+                return response()->json([
+                    "result" => false,
+                    "error_code" => 404
                 ]);
             }
+            return response()->json([
+                "result" => true
+            ]);
+
+
             //error code 403
             //null は404　
 
@@ -31,12 +43,12 @@ class LessonAttendanceController extends Controller
             return response()->json([
                 "result" => true,
                 //"lessonAttendance" => $lessonAttendance,
-                //"studen_id" => $lessonAttendance->attendance->student_id
+                "student_id" => $lessonAttendance->attendance->student_id
             ]);
         } catch (\Exception $e) {
             return response()->json([
                 "result" => false,
-                "message" => $e->getMessage() //←エラーログに残す※例外のメッセージは要らない。理由対処しきれない。メッセージを出したところでフロントでは処理しようがない。
+                Log::debug('An informational message.') //←エラーログに残す※例外のメッセージは要らない。理由対処しきれない。メッセージを出したところでフロントでは処理しようがない。
             ]);
         }
     }

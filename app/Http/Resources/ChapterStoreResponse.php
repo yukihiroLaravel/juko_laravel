7ace090d3afda49d242a0c6c620fcefff2eefe95
kishio taro <kishio@example.com>
2023-04-04 19:29:31 +0900

    空のレスポンスを返す

---
+++ app/Http/Resources/ChapterStoreResponse.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ChapterStoreResponse extends JsonResource
+{
+    public function __construct()
+    {
+        
+    }
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [] ;
+    }
+
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,7 @@ Route::prefix('v1')->group(function () {
         Route::post('register', 'Api\CourseController@store');
         Route::prefix('chapter')->group(function () {
             Route::get('/', 'Api\ChapterController@show');
+            Route::get('{id}','Api\ChapterController@index');
         });
     });
     Route::patch('lesson_attendance', 'Api\LessonAttendanceController@update');

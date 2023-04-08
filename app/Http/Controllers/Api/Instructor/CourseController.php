cff8d09dc9c9a9986d0dd11e5fb219b6a9ca2c43
yusuke okada <y.okada.2125@gmail.com>
2023-04-08 15:18:21 +0900

    講座一覧（講師側）DBからのデータ取得ロジック

--- app/Http/Controllers/Api/Instructor/CourseController.php
+++ app/Http/Controllers/Api/Instructor/CourseController.php
@@ -4,17 +4,21 @@ namespace App\Http\Controllers\Api\Instructor;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Instructor\CoursesGetResponse;
+use App\Http\Requests\Instructor\CoursesGetRequest;
+use App\Model\Course;
 
 class CourseController extends Controller
 {
     /**
      * 講師側講座一覧取得API
      *
-     * @param $instructor_id
+     * @param CoursesGetRequest $request
      * @return CoursesGetResponse
      */
-    public function index($instructor_id)
-    {
-        return new CoursesGetResponse([]);
-    }
+    public function index(CoursesGetRequest $request)
+{
+    $courses = Course::where('instructor_id', $request->instructor_id)->get();
+
+    return new CoursesGetResponse($courses);
+}
 }

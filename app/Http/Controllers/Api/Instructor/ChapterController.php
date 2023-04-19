e740088d1f42477795d8f46c52a679c2728a316c
yusuke okada <y.okada.2125@gmail.com>
2023-04-20 00:12:09 +0900

    講師側のチャプター作成　空の配列を返す

--- app/Http/Controllers/Api/Instructor/ChapterController.php
+++ app/Http/Controllers/Api/Instructor/ChapterController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers\Api\Instructor;
 
 use App\Http\Controllers\Controller;
 use App\Model\Chapter;
+use App\Http\Requests\Instructor\ChapterGetRequest;
 
 class ChapterController extends Controller
 {
     public function sort()
+    {
+        return response()->json([]);
+    }
+
+    public function store(ChapterGetRequest $request, $course_id)
     {
         return response()->json([]);
     }

37dff2e7ec0965313da56ee0c4eec8764f68dbf2
kishio taro <kishio@example.com>
2023-04-21 22:43:47 +0900

    コード修正4

--- app/Http/Resources/CourseIndexResponse.php
+++ app/Http/Resources/CourseIndexResponse.php
@@ -6,10 +6,6 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class CourseIndexResponse extends JsonResource
 {
-    public function __construct($course)
-    {
-        $this->courseId = $course;
-    }
     /**
      * Transform the resource into an array.
      *
@@ -18,26 +14,25 @@ class CourseIndexResponse extends JsonResource
      */
     public function toArray($request)
     {
-          $course = $this->courseId;
-          return [
-              'course_id' => $course->id,
-              'title' => $course->title,
-              'image' => $course->image,
-              'chapters'=> $course->chapters->map(function ($chapter) {
-                  return [
-                      'chapter_id' => $chapter->id,
-                      'title' => $chapter->title,
-                      'lessons' => $chapter->lessons->map(function ($lesson) {
-                          return [
-                            'lesson_id'=>$lesson->id,
-                            'url'=>$lesson->url,
-                            'title'=>$lesson->title,
-                            'remarks'=>$lesson->remarks,
-                          ];
-                      })
-                  ];
-              })
-          ];
+        return [
+            'course_id' => $this->resource->id,
+            'title' => $this->resource->title,
+            'image' => $this->resource->image,
+            'chapters'=> $this->resource->chapters->map(function ($chapter) {
+                return [
+                    'chapter_id' => $chapter->id,
+                    'title' => $chapter->title,
+                    'lessons' => $chapter->lessons->map(function ($lesson) {
+                        return [
+                           'lesson_id'=>$lesson->id,
+                           'url'=>$lesson->url,
+                           'title'=>$lesson->title,
+                           'remarks'=>$lesson->remarks,
+                        ];
+                    })
+                ];
+            })
+        ];
     }
 
 

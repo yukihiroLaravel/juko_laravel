9df42d1225365b97080f945453d8e106e7ff4f56
yusuke okada <y.okada.2125@gmail.com>
2023-04-25 22:00:14 +0900

    チャプター作成（講師側）DBからデータ取得ロジック

--- app/Http/Controllers/Api/Instructor/ChapterController.php
+++ app/Http/Controllers/Api/Instructor/ChapterController.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers\Api\Instructor;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Instructor\ChapterGetRequest;
-
+use App\Model\Chapter;
 
 class ChapterController extends Controller
 {
     public function store(ChapterGetRequest $request, $course_id)
     {
-        return response()->json([]);
+        $chapter = new Chapter();
+        $chapter->course_id = $course_id;
+        $chapter->title = $request->input('title');
+        $chapter->save();
+        return response()->json($chapter);
     }
 }

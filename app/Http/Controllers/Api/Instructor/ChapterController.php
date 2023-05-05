ba2c4cf87983f0a73f22640f52ea269ba8a60a24
akane goto <goto.akane.8.25@gmail.com>
2023-05-05 22:56:36 +0900

    チャプター削除機能作成

--- app/Http/Controllers/Api/Instructor/ChapterController.php
+++ app/Http/Controllers/Api/Instructor/ChapterController.php
@@ -3,18 +3,23 @@
 namespace App\Http\Controllers\Api\Instructor;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Instructor\ChapterDeleteRequest;
+use App\Model\Chapter;
 
 class ChapterController extends Controller
 {
- /**
+    /**
      * 講師側講座一覧取得API
      *
      * @param $chapter_id
      * @return CoursesGetResponse
      */ 
     
-     public function destory($chapter_id)
+     public function delete(ChapterDeleteRequest $request)
      {
-     return response()->json([]);
+        $chapter = Chapter::findOrFail($request->chapter_id);
+        $chapter->delete();
+        return response()->json([
+            "result" => true]);
      }
 }

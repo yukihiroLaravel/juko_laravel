a9e8474ae1e054086dbfb5e2282ce479a5260e26
Kou <hikkappi@yahoo.co.jp>
2023-05-06 04:06:44 +0900

    保存パスの修正。

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -9,8 +9,8 @@ class CorsMiddleware
     public function handle($request, Closure $next)
     {
         return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', '*')
-        ->header('Access-Control-Allow-Headers', 'Content-Type');
+        ->header('Access-Control-Allow-Origin', 'http://localhost:3001')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN');
     }
 }

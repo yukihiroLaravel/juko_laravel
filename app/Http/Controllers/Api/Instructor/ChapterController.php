8192c039fb67d6ae2efb84b4b1cd291fd737526c
yusuke okada <y.okada.2125@gmail.com>
2023-05-09 07:41:25 +0900

    チャプター作成（講師側）その他修正、レスポンス追加2

--- app/Http/Controllers/Api/Instructor/ChapterController.php
+++ app/Http/Controllers/Api/Instructor/ChapterController.php
@@ -3,24 +3,28 @@
 namespace App\Http\Controllers\Api\Instructor;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Instructor\ChapterPostRequest;
-use App\Http\Resources\Instructor\ChapterPostResponse;
-use Illuminate\Validation\ValidationException;
+use App\Http\Requests\Instructor\ChapterStoreRequest;
+use App\Http\Resources\Instructor\ChapterStoreResponse;
 use App\Model\Chapter;
+use App\Model\Course;
+use Exception;
+use Log;
 
 class ChapterController extends Controller
 {
     /**
      * チャプター新規作成
      *
-     * @param ChapterPostRequest $request
+     * @param ChapterStoreRequest $request
      * @param int $course_id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function store(ChapterPostRequest $request, $course_id)
+    public function store(ChapterStoreRequest $request, $course_id)
     {
-        try {
-            $validatedData = $request->validate($request->rules());
+        try{
+            $course = Course::findOrFail($course_id);
+
+            // TODO 認証ユーザーが作成した講座かどうかを検証する必要がある
 
             $chapter = Chapter::create([
                 'course_id' => $course_id,
@@ -29,14 +33,13 @@ class ChapterController extends Controller
 
             return response()->json([
                 'result' => true,
-                'data' => new ChapterPostResponse($chapter),
+                'data' => new ChapterStoreResponse($chapter),
             ]);
-        } catch (ValidationException $e) {
+        } catch (Exception $e) {
+            Log::error($e);
             return response()->json([
-                'result' => false,
-                'error_message' => 'Invalid Request Body.',
-                'error_code' => 400,
-            ], 400);
+                'result' => false
+            ], 500);
         }
     }
 }

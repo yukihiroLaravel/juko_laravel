527513a831c9ea46a3d64238dab412dae4c1d972
yusuke okada <y.okada.2125@gmail.com>
2023-05-09 22:53:36 +0900

    チャプター作成（講師側）その他、コントローラー修正

--- app/Http/Controllers/Api/Instructor/ChapterController.php
+++ app/Http/Controllers/Api/Instructor/ChapterController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Instructor\ChapterStoreRequest;
 use App\Http\Resources\Instructor\ChapterStoreResponse;
 use App\Model\Chapter;
-use App\Model\Course;
 use Exception;
 use Log;
 
@@ -16,18 +15,15 @@ class ChapterController extends Controller
      * チャプター新規作成
      *
      * @param ChapterStoreRequest $request
-     * @param int $course_id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function store(ChapterStoreRequest $request, $course_id)
+    public function store(ChapterStoreRequest $request)
     {
         try{
-            $course = Course::findOrFail($course_id);
-
             // TODO 認証ユーザーが作成した講座かどうかを検証する必要がある
 
             $chapter = Chapter::create([
-                'course_id' => $course_id,
+                'course_id' => $request->input('course_id'),
                 'title' => $request->input('title'),
             ]);
 

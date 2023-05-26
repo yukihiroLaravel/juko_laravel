c1dfc3cbe8523b07387464229399191fb87917e8
katsu <keshupinglin@gmail.com>
2023-05-27 07:13:44 +0900

    更新機能の空配列を返すようにコントローラ＋ルーティング作成

--- routes/api.php
+++ routes/api.php
@@ -45,7 +45,6 @@ Route::prefix('v1')->group(function () {
         });
     });
     
-
     // 受講生側API
     Route::prefix('course')->group(function () {
         Route::get('/', 'Api\CourseController@show');

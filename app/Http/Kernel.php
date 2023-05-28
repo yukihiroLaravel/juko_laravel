3aa11c0d83edf0817aa903b30c1afc0ff734e617
Kou <hikkappi@yahoo.co.jp>
2023-05-28 21:21:45 +0900

    Cors対応のミドルウェアを実装。

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,6 +20,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\ForceJsonResponse::class,
+        \App\Http\Middleware\CorsMiddleware::class
     ];
 
     /**

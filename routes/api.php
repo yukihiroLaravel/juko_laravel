bae99fe76991de4ab1218580fc6eb697e783db7a
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2023-05-29 00:52:16 +0900

    レッスン編集画面の空の配列を返す(コントローラーとルーティング修正)

--- routes/api.php
+++ routes/api.php
@@ -37,7 +37,6 @@ Route::prefix('v1')->group(function () {
                         Route::prefix('lesson')->group(function () {
                             Route::post('/', 'Api\Instructor\LessonController@store');
                             Route::post('sort', 'Api\Instructor\LessonController@sort');
-                            Route::get('edit', 'Api\Instructor\LessonController@edit');
                         });
                     });
                 });

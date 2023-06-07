4409261bf976bc8ed3519d54a2097dfafb407f7f
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2023-06-08 00:05:03 +0900

    チャプター詳細のリクエスト、リソース追記

---
+++ app/Http/Resources/Instructor/ChapterShowResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources\Instructor;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ChapterShowResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => [
+                'chapter_id' => $this->chapter_id,
+                'title' => $this->title,
+                'lessons' => $this->lessons->map(function ($lesson) {
+                    return [
+                        'lesson_id' => $lesson->lesson_id,
+                        'title' => $lesson->title,
+                        'url' => $lesson->url,
+                        'remark' => $lesson->remarks,
+                        'order' => $lesson->order,
+                    ];
+                }),
+            ],
+        ];
+
+    }
+}

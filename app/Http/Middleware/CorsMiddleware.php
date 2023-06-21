e12dd49e857adacde58c76e9605d2e73fa98fcc3
Kou <hikkappi@yahoo.co.jp>
2023-06-21 09:46:14 +0900

    CORSの許容メソッドにPATCHを含める。

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -10,7 +10,7 @@ class CorsMiddleware
     {
         return $next($request)
         ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN');
     }
 }

9a16d3f42cac52873a61a3353d77d9ff74703e17
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2023-07-04 11:58:28 +0900

    講座のコントローラーと編集修正、講座の登録修正

--- app/Http/Resources/Instructor/CourseStoreResource.php
+++ app/Http/Resources/Instructor/CourseStoreResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\Instructor;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class CourseEditResource extends JsonResource
+class CourseStoreResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -15,10 +15,10 @@ class CourseEditResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'course_id' => $this->resource->id,
-            'title' => $this->resource->title,
-            'image' => $this->resource->image,
-            'status' => $this->resource->private ? 'private' : 'public',
+            'course_id' => $this->id,
+            'title' => $this->title,
+            'image' => $this->image,
+            'status' => $this->status,
         ];
     }
 }

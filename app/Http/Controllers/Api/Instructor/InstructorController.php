c7a9b645a5c78a6c264de9f25e73dc629616cda7
kenta327 <kenta03271127@gmail.com>
2023-07-15 19:05:09 +0900

    レビュー後修正

--- app/Http/Controllers/Api/Instructor/InstructorController.php
+++ app/Http/Controllers/Api/Instructor/InstructorController.php
@@ -2,10 +2,9 @@
 
 namespace App\Http\Controllers\Api\Instructor;
 
-use App\Model\instructor;
+use App\Model\Instructor;
 use App\Http\Resources\Instructor\InstructorEditResource;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class InstructorController extends Controller
 {
@@ -18,8 +17,8 @@ class InstructorController extends Controller
     public function edit()
     {   
         // TODO 認証機能ができるまで、講師IDを固定値で設定
-        $instructorId = 1;
-        $Instructor = Instructor::findOrFail($instructorId);
+        $InstructorId = 1;
+        $Instructor = Instructor::findOrFail($InstructorId);
         return new InstructorEditResource($Instructor);
     }
 }

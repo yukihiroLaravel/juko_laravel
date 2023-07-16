718c0637dcd3b8d604361dd5d67a4035b107cb18
nidasi <nidasi@example.com>
2023-07-16 23:32:10 +0900

    jka-279 受講生の情報編集画面の空配列

---
+++ app/Http/Controllers/Api/Students/StudentController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api\Students;
+
+use App\Http\Controllers\Controller;
+use App\Model\Student;
+use App\Model\Attendance;
+//use App\Model\LessonAttendance;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class StudentController extends Controller
+{
+
+  /**
+   * 
+   *
+   * @param Request $request
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function edit($student_id)
+  {
+    $students = Student::findOrFail($student_id);
+
+    $attendances = Attendance::where('student_id', $student_id)->get();
+
+    //$lessonAttendances = LessonAttendance::where('student_id', $student_id)->get();
+
+    return response()->json([
+      
+    ]);
+  }
+}

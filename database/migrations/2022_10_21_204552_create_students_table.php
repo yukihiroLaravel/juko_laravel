b9f957103a0844315b64ec89231621a002919803
naoki mitamura <zango3333@yahoo.co.jp>
2023-07-30 12:13:52 +0900

    jka-300 マイグレーション・シーダー変更(三田村)

--- database/migrations/2022_10_21_204552_create_students_table.php
+++ database/migrations/2022_10_21_204552_create_students_table.php
@@ -28,6 +28,7 @@ class CreateStudentsTable extends Migration
             $table->dateTime('created_at');
             $table->dateTime('updated_at');
             $table->softDeletes();
+            $table->dateTime('last_login_at');
         });
     }
 

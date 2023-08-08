0328fce210d35bb3efd9c2fd77cb93a41af02d03
masahiro-mera <romasahiro5612@gmail.com>
2023-08-09 02:16:07 +0900

    jka-312 空の配列を返すようにコントローラ＋ルーティング作成

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
 Route::prefix('v1')->group(function () {
     // 講師側API
     Route::prefix('instructor')->group(function () {

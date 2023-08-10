e5eec8182a444c4846fc78b704bae95779695ece
yamadaman-bs <loxya3.sksy2@gmail.com>
2023-08-10 17:16:11 +0900

    レッスン 認可処理

--- app/Http/Controllers/Api/Instructor/LessonController.php
+++ app/Http/Controllers/Api/Instructor/LessonController.php
@@ -119,17 +119,20 @@ class LessonController extends Controller
                 ]);
             }
             $user = Instructor::find(1);
-            $lessons = $request->input('lessons');
-            foreach ($lessons as $lesson){
-                $lessonsSort = Lesson::with('chapter.course')->find($lesson['lesson_id']);
-                if($lessonsSort === null){
-                    // todo 無い場合は例外に投げる
+            $inputLessons = $request->input('lessons');
+            foreach ($inputLessons as $inputLesson){
+                $lesson = Lesson::with('chapter.course')->find($inputLesson['lesson_id']);
+
+                if($lesson === null){
+                    throw new Exception('Lesson not found');
                 }
-                if ((int) $request->chapter_id !== $lessonsSort->chapter->id || (int) $request->course_id !== $lessonsSort->chapter->course_id){
-                    // todo ここで失敗したら例外に投げる
+
+                if ((int) $request->chapter_id !== $lesson->chapter->id || (int) $request->course_id !== $lesson->chapter->course_id) {
+                    throw new Exception('Invalid lesson');
                 }
-                Lesson::findOrFail($lesson['lesson_id'])->update([
-                    'order' => $lesson['order']
+
+                $lesson->update([
+                    'order' => $inputLesson['order']
                 ]);
             }
             DB::commit();

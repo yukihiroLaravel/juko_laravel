6b8a94b88260db5942a661cf72fd3aa5f87b12fa
yuki ninomiya <kyuu.195@gmail.com>
2023-08-11 19:23:52 +0900

    講座受講生登録（講師側）で登録した際にstudentsテーブルに受講生データを追加：DBへのロジック作成

--- app/Http/Controllers/Api/Instructor/StudentController.php
+++ app/Http/Controllers/Api/Instructor/StudentController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Api\Instructor;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Model\Student;
+use Carbon\Carbon;
 
 class StudentController extends Controller
 {
@@ -16,6 +18,31 @@ class StudentController extends Controller
 
     public function store(Request $request)
     {
-        return response()->json([]);
+        $student = null;
+        if ( Student::where('email', $request->email)->first() === null ) {
+            $student = Student::create([
+                // 'given_name_by_instructor' => $request->name, まだ実装されてないのでnick_nameで代用
+                'nick_name' => $request->nick_name,
+                'email' => $request->email,
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]);
+        } else {
+            return response()->json([
+                'result' => false,
+                "error_message" => "Invalid email.",
+                "error_code" => "400"
+            ]);
+        }
+        
+        return response()->json([
+            'result' => true,
+            'data' => [
+                'id' => $student->id,
+                // $student->given_name_by_instructor, まだ実装されてないのでnick_nameで代用
+                'nick_name' => $student->nick_name,
+                'email' => $student->email
+            ]
+        ]);
     }
 }

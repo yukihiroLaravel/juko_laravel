c6d8909d8be992c9313bb2932768e6110e3a4be1
Shinichiro-Nuno <nuno1216shin@gmail.com>
2023-08-11 11:17:09 +0900

    空の配列を返すようにコントローラ＋ルーティング作成(ブランチ名を英語に変更)

---
+++ app/Http/Controllers/Api/Instructor/AttendancesController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\Instructor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AttendancesController extends Controller
+{
+    public function store(Request $request)
+    {
+        return response()->json([]);
+    }
+}

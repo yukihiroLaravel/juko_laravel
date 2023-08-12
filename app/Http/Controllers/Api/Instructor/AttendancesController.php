9f1d078a5e534d1f109baece10d513dc6269f346
Shinichiro-Nuno <nuno1216shin@gmail.com>
2023-08-12 12:15:16 +0900

    DBへのロジック作成

--- app/Http/Controllers/Api/Instructor/AttendancesController.php
+++ app/Http/Controllers/Api/Instructor/AttendancesController.php
@@ -3,12 +3,32 @@
 namespace App\Http\Controllers\Api\Instructor;
 
 use App\Http\Controllers\Controller;
+use App\Model\Attendance;
 use Illuminate\Http\Request;
 
 class AttendancesController extends Controller
 {
     public function store(Request $request)
     {
-        return response()->json([]);
+        $validateData = $request->validate([
+            'course_id' => 'required|exists:courses,id',
+            'student_id' => 'required|exists:students,id',
+        ]);
+
+        $existingAttendance = Attendance::where('course_id', $validateData['course_id'])
+            ->where('student_id', $validateData['student_id'])
+            ->first();
+
+        if ($existingAttendance) {
+            return response()->json([
+                'error' => '既に登録されています'], 409);
+        }
+
+        $attendance = new Attendance();
+        $attendance->course_id = $validateData['course_id'];
+        $attendance->student_id = $validateData['student_id'];
+        $attendance->save();
+
+        return response()->json(['message' => '登録しました'], 201);
     }
 }

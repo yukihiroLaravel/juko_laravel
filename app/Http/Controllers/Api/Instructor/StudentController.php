950e65628022fe8901acca13875f7ca6fc8789e3
tomoki amemiya <tomoki423@outlook.com>
2023-08-13 18:05:16 +0900

    jka-310受講生一覧画面（講師側）DBへのロジック作成（受講生情報取得実装）

--- app/Http/Controllers/Api/Instructor/StudentController.php
+++ app/Http/Controllers/Api/Instructor/StudentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\Instructor;
 
+use App\Model\Attendance;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -9,6 +10,31 @@ class StudentController extends Controller
 {
     public function index(Request $request)
     {
-        return response()->json([]);
+        $attendances = Attendance::where('course_id', $request->course_id)->get();
+
+        $students = [];
+        
+        foreach ($attendances as $attendance) {
+            $student = $attendance->student;
+            
+            $students[] = [
+                'id' => $student->id,
+                'nick_name' => $student->nick_name,
+                'email' => $student->email,
+                'course_title' => $attendance->course->title,
+                'attendanced_at' => $attendance->created_at->format('Y/m/d'),
+            ];
+        }
+
+        return response()->json([
+            'data' => [
+                'course' => [
+                    'id' => $attendances->first()->course->id,
+                    'image' => $attendances->first()->course->image,
+                    'title' => $attendances->first()->course->title,
+                ],
+                'students' => $students,
+            ],
+        ]);
     }
 }

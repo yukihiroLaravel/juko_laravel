73c79f28dc8819d94261279a0936c41cb3bbc0c8
yuki ohashi <yukiohashi1119@gmail.com>
2023-08-14 23:12:03 +0900

    JKA-289 ダッシュボード(講師側)講座の受講状況のDBへのロジック作成(ユウキ)

--- app/Http/Controllers/Api/Instructor/AttendanceController.php
+++ app/Http/Controllers/Api/Instructor/AttendanceController.php
@@ -3,11 +3,41 @@
 namespace App\Http\Controllers\Api\Instructor;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Model\Attendance;
+use App\Model\Chapter;
 
 class AttendanceController extends Controller
 {
     public function show() {
-        return response()->json([]);
+        $courseId = 1;
+        $chapters = [];
+        $chapterData = Chapter::where('course_id', $courseId)
+            ->with(['lessons' => function ($query) {
+                $query->with('lessonAttendance');
+            }])
+            ->get();
+        $studentsCount = Attendance::where('course_id', $courseId)->count();
+
+        foreach ($chapterData as $chapter) {
+            $completedCount = 0;
+            $chapterId = $chapter->id;
+            $title = $chapter->title;
+            $chapterInfo = ['chapter_id' => $chapterId, 'title' => $title];
+            foreach ($chapter->lessons as $lesson) {
+                foreach ($lesson->lessonAttendance as $lessonAttendance) {
+                    $status = $lessonAttendance->status;
+                    if($status === 'completed_attendance'){
+                        $completedCount+=1;
+                    }
+                }
+            }
+            $chapterInfo['completed_count'] = empty($chapter->lessons) ? 0 : $completedCount;
+            $chapters[] = $chapterInfo;
+        }
+        
+        return response()->json([
+            "chapters" => $chapters,
+            "students_count" => $studentsCount,
+        ]);
     }
 }

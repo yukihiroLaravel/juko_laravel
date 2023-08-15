1d0cdbfc5f9e0b360a5adaca43068938345d4621
yuki ohashi <yukiohashi1119@gmail.com>
2023-08-16 00:16:03 +0900

    JKA-289 ダッシュボード(講師側)講座の受講状況のDBへのロジック作成_指摘事項修正(ユウキ)

--- app/Http/Controllers/Api/Instructor/AttendanceController.php
+++ app/Http/Controllers/Api/Instructor/AttendanceController.php
@@ -4,18 +4,21 @@ namespace App\Http\Controllers\Api\Instructor;
 
 use App\Http\Controllers\Controller;
 use App\Model\Attendance;
+use App\Model\LessonAttendance;
 use App\Model\Chapter;
 
 class AttendanceController extends Controller
 {
-    public function show() {
-        $courseId = 1;
+    /**
+     * 受講状況取得API
+     *
+     * @param 
+     * @return 
+     */
+    public function show($course_id) {
+        $courseId = $course_id;
         $chapters = [];
-        $chapterData = Chapter::where('course_id', $courseId)
-            ->with(['lessons' => function ($query) {
-                $query->with('lessonAttendance');
-            }])
-            ->get();
+        $chapterData = Chapter::with('lessons.lessonAttendances')->where('course_id', $courseId)->get();
         $studentsCount = Attendance::where('course_id', $courseId)->count();
 
         foreach ($chapterData as $chapter) {
@@ -24,14 +27,14 @@ class AttendanceController extends Controller
             $title = $chapter->title;
             $chapterInfo = ['chapter_id' => $chapterId, 'title' => $title];
             foreach ($chapter->lessons as $lesson) {
-                foreach ($lesson->lessonAttendance as $lessonAttendance) {
+                foreach ($lesson->lessonAttendances as $lessonAttendance) {
                     $status = $lessonAttendance->status;
-                    if($status === 'completed_attendance'){
+                    if($status === LessonAttendance::STATUS_COMPLETED_ATTENDANCE){
                         $completedCount+=1;
                     }
                 }
             }
-            $chapterInfo['completed_count'] = empty($chapter->lessons) ? 0 : $completedCount;
+            $chapterInfo['completed_count'] = $chapter->lessons->isEmpty() ? 0 : $completedCount;
             $chapters[] = $chapterInfo;
         }
         

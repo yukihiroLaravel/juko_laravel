c37fff15bfed50523e5a83846b6becf64e754052
naoki mitamura <zango3333@yahoo.co.jp>
2023-08-16 12:32:39 +0900

    jka-328 動作確認用空配列(修正３)

---
+++ app/Http/Controllers/Api/Instructor/StudentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Instructor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+  public function student_test(Request $request){
+    return response()->json([]);
+  }
+}

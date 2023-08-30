383d58c5da97bf2a9dccf73a40d362a4242dbc3c
u0222 <ootayy411@gmail.com>
2023-08-31 00:20:36 +0900

    プルリク修正１回目

--- app/Model/Notification.php
+++ app/Model/Notification.php
@@ -15,4 +15,25 @@ class Notification extends Model
      * @var string
      */
     protected $table = 'notifications';
+
+    // type定数
+    const TYPE_ALWAYS = 1;
+    const TYPE_ONCE = 2;
+    const ALWAYS = 'always';
+    const ONCE = 'once';    
+    
+    /**
+     * typeをalwaysかonceで取得
+     *
+     * @return string
+     */
+    public function getTypeAttribute($value)
+    {    
+        if ($value === self::TYPE_ALWAYS) {
+            return self::ALWAYS;
+        } elseif ($value === self::TYPE_ONCE) {
+            return self::ONCE;
+        }
+        return null;
+    }
 }
--- database/migrations/2023_08_28_232815_create_notifications_table.php
+++ database/migrations/2023_08_28_232815_create_notifications_table.php
@@ -18,11 +18,12 @@ class CreateNotificationsTable extends Migration
             $table->bigInteger('course_id')->unsigned();
             $table->bigInteger('instructor_id')->unsigned();
             $table->string('title', 50)->comment('タイトル');
-            $table->tinyInteger('type')->comment('表示パターン区分'); //1は"always"（期間中ずっと表示）2は"once"（1度のみ表示）として定数にして扱う
+            $table->boolean('type')->comment('表示パターン区分');
             $table->dateTime('start_date')->comment('開始日時');
             $table->dateTime('end_date')->comment('終了日時');
             $table->text('content')->comment('本文');
-            $table->timestamps();
+            $table->dateTime('created_at');
+            $table->dateTime('updated_at');
             $table->softDeletes();
         });
     }

b73492f29526f9bc3dbec285766a25a0b4303919
yuki ohashi <yukiohashi1119@gmail.com>
2023-09-02 22:07:58 +0900

    JKA-377 空配列を返すコントローラ＋ルーティング作成（ユウキ）

---
+++ app/Http/Controllers/Api/Instructor/NotificationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\Instructor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NotificationController extends Controller
+{
+    /**
+     * お知らせ表示更新API
+     * @param
+     * @return
+     */
+    public function update() {
+        return response()->json([]);
+    }
+}

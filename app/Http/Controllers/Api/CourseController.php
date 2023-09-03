001cd31e3c99a69d6bc904a8bb9f5401feb51012
yuki ninomiya <kyuu.195@gmail.com>
2023-09-03 10:45:38 +0900

    レビューされた内容の修正

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -10,7 +10,6 @@ use App\Http\Resources\CourseIndexResource;
 use App\Http\Resources\CourseShowResource;
 use App\Model\Attendance;
 use App\Model\Course;
-use App\Model\Chapter;
 use App\Model\LessonAttendance;
 
 class CourseController extends Controller
@@ -82,62 +81,94 @@ class CourseController extends Controller
         ])
         ->firstOrFail();
 
-        // 終了済みのチャプター数
-        $completedLessonAttendanceIds = $attendance->lessonAttendances->filter(function ($lessonAttendance) {
-            return $lessonAttendance->status === LessonAttendance::STATUS_COMPLETED_ATTENDANCE;
-        })
-        ->pluck('id');
-        
-        $groupedLessons = $attendance->course->chapters
-        ->flatMap(function ($chapter) {
-            return $chapter->lessons->groupBy('chapter_id');
-        });
-
-        $completedChaptersCount = $groupedLessons->filter(function ($groupedLesson) use ($completedLessonAttendanceIds) {
-            return $groupedLesson->pluck('id')->intersect($completedLessonAttendanceIds)->count() === $groupedLesson->count();
-        })->count();
-
-        // チャプター合計
-        $totalChaptersCount = $attendance->course->chapters->count();
-
-        // 終了済みのレッスン数
-        $completedLessonsCount = 0;
-        foreach ($attendance->lessonAttendances as $lessonAttendance)
-        if ($lessonAttendance->status === LessonAttendance::STATUS_COMPLETED_ATTENDANCE) {
-            $completedLessonsCount ++;
-        }
-        
-        // レッスン合計
-        $totalLessonsCount = 0;
-        foreach ($attendance->course->chapters as $chapter) {
-            $lessonCount = $chapter->lessons->count();
-            $totalLessonsCount += $lessonCount; 
-        }
-
-        // 未完了のチャプターでIDが最も若いレッスン（続きのレッスンID取得）
-        $UnCompletedLessonAttendanceIds = $attendance->lessonAttendances->filter(function ($lessonAttendance) {
-            return $lessonAttendance->status !== LessonAttendance::STATUS_COMPLETED_ATTENDANCE;
-        })
-        ->pluck('id');
-        
-        $youngestUnCompletedLesson = $attendance->course->chapters
-        ->flatMap(function ($chapter) use ($UnCompletedLessonAttendanceIds) {
-            return $chapter->lessons->filter(function ($lesson) use ($UnCompletedLessonAttendanceIds) {
-                return $UnCompletedLessonAttendanceIds->contains($lesson->id);
-            });
-        })
-        ->sortBy('chapter_id')->first()['id'];
-
         return response()->json([
             'course' =>[
                 'course_id' => $request->course_id,
                 'progress' => $attendance->progress
             ],
-            "number_of_completed_chapters" => $completedChaptersCount,
-            "number_of_total_chapters" => $totalChaptersCount,
-            "number_of_completed_lessons" => $completedLessonsCount,
-            "number_of_total_lessons" => $totalLessonsCount,
-            "continue_lesson_id" => $youngestUnCompletedLesson
+            "number_of_completed_chapters" => $this->getCompletedChaptersCount($attendance),
+            "number_of_total_chapters" => $this->getTotalChaptersCount($attendance),
+            "number_of_completed_lessons" => $this->getCompletedLessonsCount($attendance),
+            "number_of_total_lessons" => $this->getTotalLessonsCount($attendance),
+            "continue_lesson_id" => $this->getYoungestUnCompletedLesson($attendance)
         ]); 
+    }
+    /**
+     * 完了済みのチャプター数を取得する
+     *
+     * @param Attendance $attendance
+     */
+    private function getCompletedChaptersCount($attendance)
+    {
+        return $attendance->course->chapters->filter(function ($chapter) use ($attendance) {
+            $isCompleted = false;
+            // 全てのレッスンが完了済みかどうかをチェック
+            $chapter->lessons->each(function ($lesson) use ($attendance, &$isCompleted) {
+                $lessonAttendance = $attendance->lessonAttendances->where('lesson_id', $lesson->id)->first();
+                if ($lessonAttendance->status !== LessonAttendance::STATUS_COMPLETED_ATTENDANCE) {
+                    $isCompleted = false;
+                    return false;
+                }
+                $isCompleted = true;
+            });
+            return $isCompleted;
+        })->count();        
+    }
+    /**
+     * チャプター合計を取得する
+     *
+     * @param Attendance $attendance
+     */
+    private function getTotalChaptersCount($attendance)
+    {
+        return $attendance->course->chapters->count();
+    }
+    /**
+     * 完了済みのレッスン数を取得する
+     *
+     * @param Attendance $attendance
+     */
+    private function getCompletedLessonsCount($attendance)
+    {
+        return $attendance->lessonAttendances->filter(function ($lessonAttendance) {
+            return $lessonAttendance->status === LessonAttendance::STATUS_COMPLETED_ATTENDANCE;
+        })->count();
+    }
+    /**
+     * レッスン合計を取得する
+     *
+     * @param Attendance $attendance
+     */
+    private function getTotalLessonsCount($attendance)
+    {
+        $totalLessonsCount = 0;
+        foreach ($attendance->course->chapters as $chapter) {
+            $lessonCount = $chapter->lessons->count();
+            $totalLessonsCount += $lessonCount; 
+        }
+        return $totalLessonsCount;
+    }
+    /**
+     * 続きのレッスンIDを取得する（IDが最も若い未完了のチャプターの内、IDが最も若い未完了のレッスン）
+     *
+     * @param Attendance $attendance
+     */
+    private function getYoungestUnCompletedLesson($attendance)
+    {
+        $UnCompletedLessonAttendanceIds = $attendance->lessonAttendances->filter(function ($lessonAttendance) {
+            return $lessonAttendance->status !== LessonAttendance::STATUS_COMPLETED_ATTENDANCE;
+        })
+        ->pluck('id');
+        // 全てのレッスンが完了済みの場合
+        if ($UnCompletedLessonAttendanceIds->isEmpty()) {
+            return null;
+        }
+
+        return $attendance->course->chapters->flatMap(function ($chapter) use ($UnCompletedLessonAttendanceIds) {
+            return $chapter->lessons->filter(function ($lesson) use ($UnCompletedLessonAttendanceIds) {
+                return $UnCompletedLessonAttendanceIds->contains($lesson->id);
+            });
+        })
+        ->sortBy('chapter_id')->first()['id'];
     }
 }

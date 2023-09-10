b9d83803b4548af5d4560df2a422a91b59c041ba
daiki morishima <mighty19960220d@gmail.com>
2023-09-10 23:11:23 +0900

    JKA-412 お知らせ編集機能 表示GET部分のみ（講師側）DBへのロジック作成

--- app/Http/Controllers/Api/Instructor/NotificationController.php
+++ app/Http/Controllers/Api/Instructor/NotificationController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers\Api\Instructor;
 
 use App\Http\Controllers\Controller;
+use App\Model\Notification; // Notificationモデルをインポート
 use Illuminate\Http\Request;
 
 class NotificationController extends Controller
 {
-    public function show(Request $request)
+    public function show(Request $request, $notification_id)
     {
-        return response()->json([]);
+        // 通知情報をデータベースから取得
+        $notification = Notification::find($notification_id);
+
+        if (!$notification) {
+            return response()->json([
+                "result" => "false",
+                "error_code" => 400,
+                "error_message" => "Bad request."
+            ], 400);
+        }
+
+        return response()->json($notification);
     }
 }

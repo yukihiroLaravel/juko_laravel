08d35eb6241490301413a66e38ffed316c46cb10
u0222 <ootayy411@gmail.com>
2023-09-10 23:07:15 +0900

    jka-408 チャプター作成機能の画面修正（講師側）Resourseファイル変更

--- app/Http/Resources/Instructor/CourseShowResource.php
+++ app/Http/Resources/Instructor/CourseShowResource.php
@@ -23,6 +23,7 @@ class CourseShowResource extends JsonResource
                 return [
                     'chapter_id' => $chapter->id,
                     'title' => $chapter->title,
+                    'status' => $chapter->status,
                     'lessons' => $chapter->lessons->map(function ($lesson) {
                         return [
                            'lesson_id'=>$lesson->id,

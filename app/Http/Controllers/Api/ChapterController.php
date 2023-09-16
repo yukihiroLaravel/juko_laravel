e6eb7d1931783a77265e8eb315cc76c711222dea
u0222 <ootayy411@gmail.com>
2023-09-17 08:25:09 +0900

    受講生側のチャプター取得APIは、公開状態のチャプターのみを取得：DBロジックへの変更

--- app/Http/Controllers/Api/ChapterController.php
+++ app/Http/Controllers/Api/ChapterController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\ChapterGetRequest;
 use App\Http\Resources\ChapterShowResource;
 use App\Model\Attendance;
 use Symfony\Component\HttpKernel\Exception\HttpException;
+use App\Model\Chapter;
 
 class ChapterController extends Controller
 {
@@ -30,6 +31,21 @@ class ChapterController extends Controller
             throw new HttpException(404, "Not found attendance.");
         }
 
+        $publicChapters = $this->extractPublicChapter($attendance->course->chapters);
+        $attendance->course->chapters = $publicChapters;
         return new ChapterShowResource($attendance, $request->chapter_id);
+    }
+
+    /**
+     * 公開中のチャプターを抽出
+     *
+     * @param \Illuminate\Support\Collection $chapters
+     * @return \Illuminate\Support\Collection
+     */
+    private function extractPublicChapter($chapters)
+    {
+        return $chapters->filter(function ($chapter) {
+            return $chapter->status === Chapter::STATUS_PUBLIC;
+        });
     }
 }

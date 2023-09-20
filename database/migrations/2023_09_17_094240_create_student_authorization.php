ae2b43daa691b9f84e3e1d1fbaf64af31c285779
Shogo Uozumi <square.7.podod@gmail.com>
2023-09-20 22:46:31 +0900

    ユニーク制約の追加

--- database/migrations/2023_09_17_094240_create_student_authorization.php
+++ database/migrations/2023_09_17_094240_create_student_authorization.php
@@ -15,7 +15,7 @@ class CreateStudentAuthorization extends Migration
     {
         Schema::create('student_authorization', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('student_id')->unsigned()->comment('仮登録生徒識別ID');
+            $table->bigInteger('student_id')->unsigned()->unique()->comment('仮登録生徒識別ID');
             $table->bigInteger('trial_count')->comment('試行回数');
             $table->string('code')->unique()->comment('認証コード');
             $table->dateTime('expire_at')->comment('認証コード有効期間');

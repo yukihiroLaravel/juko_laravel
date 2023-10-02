7768e8ccbafcc1af200194443a62932d50cfceb4
seiichi ozawa <sasutinaburu@yahoo.co.jp>
2023-10-02 11:00:19 +0900

    JKA-419お知らせ削除機能(講師側)/ ロジック修正(seiichi)

--- routes/api.php
+++ routes/api.php
@@ -38,6 +38,7 @@ Route::prefix('v1')->group(function () {
         Route::prefix('attendance')->group(function () {
             Route::post('/', 'Api\Instructor\AttendanceController@store');
         });
+        Route::delete('notification/{notification_id}','Api\Instructor\NotificationController@delete');
         Route::patch('/', 'Api\Instructor\InstructorController@update');
         Route::patch('notification/{notification_id}', 'Api\Instructor\NotificationController@update');
         Route::post('student', 'Api\Instructor\StudentController@store');

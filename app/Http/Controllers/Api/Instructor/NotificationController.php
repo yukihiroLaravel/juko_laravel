5ef2c41fddad48d59caec84569fa3bf3c27c3e26
Kou <hikkappi@yahoo.co.jp>
2023-10-02 18:56:43 +0900

    競合解決。

--- app/Http/Controllers/Api/Instructor/NotificationController.php
+++ app/Http/Controllers/Api/Instructor/NotificationController.php
@@ -1,13 +1,57 @@
 <?php
+
 namespace App\Http\Controllers\Api\Instructor;
+
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Instructor\NotificationIndexRequest;
+use App\Http\Resources\Instructor\NotificationIndexResource;
+use App\Http\Requests\Instructor\NotificationShowRequest;
+use App\Http\Resources\Instructor\NotificationShowResource;
 use App\Http\Requests\Instructor\NotificationStoreRequest;
-use App\Model\Notification;
-use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Instructor\NotificationUpdateRequest;
+use App\Http\Resources\Instructor\NotificationUpdateResource;
 use App\Http\Requests\Instructor\NotificationDeleteRequest;
+use App\Model\Notification;
 
 class NotificationController extends Controller
 {
+   /**
+    * 講師側お知らせ一覧取得API
+    *
+    * @param NotificationIndexRequest $request
+    * @return NotificationIndexResource
+    */
+   public function index(NotificationIndexRequest $request)
+   {
+       $perPage = $request->input('per_page', 20);
+       $page = $request->input('page', 1);
+
+       $notifications = Notification::with(['course'])
+                                       ->where('instructor_id', 1) //講師IDは仮で1を指定
+                                       ->paginate($perPage, ['*'], 'page', $page);
+
+       return new NotificationIndexResource($notifications);
+   }
+
+   /**
+    * お知らせ詳細
+    *
+    * @param NotificationShowRequest $request
+    * @return NotificationShowResource
+    */
+   public function show(NotificationShowRequest $request)
+   {
+       $notification = Notification::findOrFail($request->notification_id);
+
+       return new NotificationShowResource($notification);
+   }
+
+   /**
+    * お知らせ登録
+    *
+    * @param NotificationStoreRequest $request
+    * @return \Illuminate\Http\JsonResponse
+    */
     public function store(NotificationStoreRequest $request)
     {
         Notification::create([
@@ -19,25 +63,49 @@ class NotificationController extends Controller
             'end_date'      => $request->end_date,
             'content'       => $request->content,
         ]);
+
         return response()->json([
             'result' => true,
         ]);
     }
 
-    public function delete(NotificationDeleteRequest $request)
-    {
-     try{
+    /**
+     * お知らせ更新API
+     *
+     * @param   NotificationUpdateRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(NotificationUpdateRequest $request) {
         $notification = Notification::findOrFail($request->notification_id);
-        $notification->delete();
+        $notification->fill([
+            'type'  => $request->type,
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date,
+            'title' => $request->title,
+            'content' => $request->content,
+        ])
+        ->save();
 
         return response()->json([
             'result' => true,
+            'data' => new NotificationUpdateResource($notification),
         ]);
-     } catch (\Exception $e) {
-        return response()->json([
-            'result' => false,
-            'message' => 'Notification not found',
-        ]);
-     }
+    }
+
+    public function delete(NotificationDeleteRequest $request)
+    {
+        try{
+            $notification = Notification::findOrFail($request->notification_id);
+            $notification->delete();
+
+            return response()->json([
+                'result' => true,
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'result' => false,
+                'message' => 'Notification not found',
+            ]);
+        }
     }
 }

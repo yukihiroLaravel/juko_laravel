ddda095c571dac1600721f91d772c627ed82959b
Shogo Uozumi <square.7.podod@gmail.com>
2023-10-02 10:07:46 +0900

    formrequestの作成

---
+++ app/Http/Requests/Student/StudentPostRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\Student;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\SexRule;
+
+class StudentPostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nick_name' => ['required', 'string'],
+            'last_name' => ['required', 'string'],
+            'first_name' => ['required', 'string'],
+            'email' => ['required', 'email'],
+            'occupation' => ['required', 'string'],
+            'purpose' => ['required', 'string'],
+            'birth_date' => ['required', 'date_format:Y-m-d'],
+            'sex' => ['required', 'string', new SexRule()],
+            'address' => ['required', 'string'],
+        ];
+    }
+}

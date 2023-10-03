2e29fb6975438344a79f5910bcca55a5f2873acc
Shogo Uozumi <square.7.podod@gmail.com>
2023-10-03 23:08:27 +0900

    半角スペースの修正

--- app/Http/Resources/Student/StudentPostResource.php
+++ app/Http/Resources/Student/StudentPostResource.php
@@ -15,15 +15,15 @@ class StudentPostResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'nick_name' => $this->nick_name,
-            'last_name' => $this->last_name,
+            'nick_name'  => $this->nick_name,
+            'last_name'  => $this->last_name,
             'first_name' => $this->first_name,
             'occupation' => $this->occupation,
-            'email' => $this->email,
-            'purpose' => $this->purpose,
+            'email'      => $this->email,
+            'purpose'    => $this->purpose,
             'birth_date' => $this->birth_date,
-            'sex' => $this->sex,
-            'address' => $this->address,
+            'sex'        => $this->sex,
+            'address'    => $this->address,
         ];
     }
 }

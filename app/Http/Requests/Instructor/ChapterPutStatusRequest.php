22ac939d3eb346e573bd6d9cdebcf7260a2d05da
Shogo Uozumi <square.7.podod@gmail.com>
2023-10-09 17:26:37 +0900

    フォームリクエストの作成

---
+++ app/Http/Requests/Instructor/ChapterPutStatusRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\Instructor;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\ChapterStatusRule;
+
+class ChapterPutStatusRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'status' => ['required', 'string', new ChapterStatusRule()],
+        ];
+    }
+}

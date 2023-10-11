2d52a0f2afa0dd35a2e26846befdf5b5cf42cf86
daiki tomomatsu <kurabutouryuumon@yahoo.co.jp>
2023-10-12 07:33:46 +0900

    LessonControllerのstoreメソッドのロジックをmaxメソッドを使って修正し、orderカラム最大値を取得して変数に格納。 LessonStoreResourceクラス内のtoArrayメソッドの戻り値の値で不要もなものを削除。

--- app/Http/Controllers/Api/Instructor/LessonController.php
+++ app/Http/Controllers/Api/Instructor/LessonController.php
@@ -29,12 +29,13 @@ class LessonController extends Controller
      */
     public function store(LessonStoreRequest $request)
     {
+        $maxOrder = Lesson::where('chapter_id', $request->input('chapter_id'))->max('order');
         try {
             $lesson = Lesson::create([
                 'chapter_id' => $request->input('chapter_id'),
                 'title' => $request->input('title'),
                 'status' => Lesson::STATUS_PRIVATE,
-                'order' => Lesson::where('chapter_id', $request->input('chapter_id'))->get()->count() + 1
+                'order' => (int)$maxOrder + 1,
             ]);
 
             return response()->json([
@@ -48,7 +49,7 @@ class LessonController extends Controller
             ], 500);
         }
     }
-
+    
     public function update(LessonUpdateRequest $request)
     {
         $user = Instructor::find(1);

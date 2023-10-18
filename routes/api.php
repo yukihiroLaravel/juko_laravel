95764e0f93c49e2319fae7c66659a7b5a8ab20df
yuuya narisawa <adjgmptw1497@yahoo.co.jp>
2023-10-18 17:45:15 +0900

    #287ログイン状態を各APIに反映する【各画面】修正

--- routes/api.php
+++ routes/api.php
@@ -46,8 +46,9 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
                             Route::prefix('lesson')->group(function () {
                                 Route::post('sort', 'Api\Instructor\LessonController@sort');
                                 Route::prefix('{lesson_id}')->group(function () {
-                                    Route::delete('/', 'Api\Instructor\LessonController@delete');
+                                    Route::post('/', 'Api\Instructor\LessonController@store');
                                     Route::patch('/', 'Api\Instructor\LessonController@update');
+                                    Route::delete('/', 'Api\Instructor\LessonController@delete');
                                 });
                             });
                         });
@@ -59,58 +60,50 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
     Route::get('student/edit', 'Api\Student\StudentController@edit');
 });
 
-Route::prefix('v1')->group(function () {
-    // 講師側API
-    Route::prefix('instructor')->group(function () {
-        Route::get('edit', 'Api\Instructor\InstructorController@edit');
-        Route::get('student/{student_id}', 'Api\Instructor\StudentController@show');
-
-        Route::prefix('attendance')->group(function () {
-            Route::post('/', 'Api\Instructor\AttendanceController@store');
-        });
-        Route::patch('notification/{notification_id}', 'Api\Instructor\NotificationController@update');
-        Route::post('student', 'Api\Instructor\StudentController@store');
-        Route::prefix('notification')->group(function () {
-            Route::get('index', 'Api\Instructor\NotificationController@index');
-            Route::prefix('{notification_id}')->group(function () {
-                Route::get('/', 'Api\Instructor\NotificationController@show');
-            });
-        });
-        Route::prefix('course')->group(function () {
-            Route::get('index', 'Api\Instructor\CourseController@index');
-            Route::prefix('{course_id}')->group(function () {
-                Route::get('student/index', 'Api\Instructor\StudentController@index');
-                Route::get('/', 'Api\Instructor\CourseController@show');
-                Route::get('edit', 'Api\Instructor\CourseController@edit');
-                Route::post('/', 'Api\Instructor\CourseController@update');
-                Route::prefix('notification')->group(function () {
-                    Route::post('/', 'Api\Instructor\NotificationController@store');
-                });
-                Route::prefix('attendance')->group(function () {
-                    Route::get('status', 'Api\Instructor\AttendanceController@show');
-                });
-                Route::prefix('chapter')->group(function () {
-                    Route::post('/', 'Api\Instructor\ChapterController@store');
-                    Route::post('sort', 'Api\Instructor\ChapterController@sort');
-                    Route::prefix('{chapter_id}')->group(function () {
-                        Route::get('/', 'Api\Instructor\ChapterController@show');
-                        Route::patch('/', 'Api\Instructor\ChapterController@update');
-                        Route::patch('status', 'Api\Instructor\ChapterController@updateStatus');
-                        Route::delete('/', 'Api\Instructor\ChapterController@delete');
-                        Route::prefix('lesson')->group(function () {
-                            Route::post('/', 'Api\Instructor\LessonController@store');
-                            Route::delete('/', 'Api\Instructor\LessonController@delete');
-                            Route::patch('/', 'Api\Instructor\LessonController@update');
-                        });
-                    });
-                });
-            });
+// 講師側API
+Route::prefix('instructor')->group(function () {
+    Route::get('edit', 'Api\Instructor\InstructorController@edit');
+    Route::get('student/{student_id}', 'Api\Instructor\StudentController@show');
+    Route::prefix('attendance')->group(function () {
+        Route::post('/', 'Api\Instructor\AttendanceController@store');
+    });
+    Route::patch('notification/{notification_id}', 'Api\Instructor\NotificationController@update');
+    Route::post('student', 'Api\Instructor\StudentController@store');
+    Route::prefix('notification')->group(function () {
+        Route::get('index', 'Api\Instructor\NotificationController@index');
+        Route::prefix('{notification_id}')->group(function () {
+            Route::get('/', 'Api\Instructor\NotificationController@show');
         });
     });
-
-    Route::patch('lesson_attendance', 'Api\LessonAttendanceController@update');
-
-    Route::prefix('student')->group(function () {
-        Route::patch('/', 'Api\Student\StudentController@update');
+    Route::prefix('course')->group(function () {
+        Route::get('index', 'Api\Instructor\CourseController@index');
+        Route::prefix('{course_id}')->group(function () {
+            Route::get('student/index', 'Api\Instructor\StudentController@index');
+            Route::get('/', 'Api\Instructor\CourseController@show');
+            Route::get('edit', 'Api\Instructor\CourseController@edit');
+            Route::post('/', 'Api\Instructor\CourseController@update');
+            Route::prefix('notification')->group(function () {
+                Route::post('/', 'Api\Instructor\NotificationController@store');
+            });
+            Route::prefix('attendance')->group(function () {
+                Route::get('status', 'Api\Instructor\AttendanceController@show');
+            });
+            Route::prefix('chapter')->group(function () {
+                Route::post('/', 'Api\Instructor\ChapterController@store');
+                Route::prefix('{chapter_id}')->group(function () {
+                    Route::get('/', 'Api\Instructor\ChapterController@show');
+                    Route::patch('/', 'Api\Instructor\ChapterController@update');
+                    Route::patch('status', 'Api\Instructor\ChapterController@updateStatus');
+                    Route::delete('/', 'Api\Instructor\ChapterController@delete');
+                });
+            });
+        });
     });
 });
+
+Route::patch('lesson_attendance', 'Api\LessonAttendanceController@update');
+
+Route::prefix('student')->group(function () {
+    Route::patch('/', 'Api\Student\StudentController@update');
+});
+

f2518c436f7531f64c36791973323102cc74b0cc
ma34vn <makino_vn@yahoo.co.jp>
2023-10-30 21:04:02 +0900

    💩 JKA-526：config の読み込み先をcacheからappに修正

--- resources/views/AuthenticationConfirmationMail.blade.php
+++ resources/views/AuthenticationConfirmationMail.blade.php
@@ -7,6 +7,6 @@
     <p>{{ $fullName }}さん</p>
     <p>あなたの認証コードは{{ $code }}です。認証コード有効時間は１時間です。</p>
     <p>以下のURLにアクセスして、認証を行ってください。</p>
-    <p>{{ config('cache.allow_origin') }}/verification/{{ $token }}</p>
+    <p>{{ config('app.allow_origin') }}/verification/{{ $token }}</p>
 </body>
 </html>

b7efa65d7f09fda9e65956cc2fcc3bc5b9927a19
yuuya narisawa <adjgmptw1497@yahoo.co.jp>
2023-11-06 19:23:44 +0900

    プロフィール画像返り値修正

--- app/Http/Requests/Student/StudentPatchRequest.php
+++ app/Http/Requests/Student/StudentPatchRequest.php
@@ -34,7 +34,7 @@ class StudentPatchRequest extends FormRequest
             'birth_date' => ['required', 'date_format:Y-m-d'],
             'sex' => ['required', 'string', new SexRule()],
             'address' => ['required', 'string'],
-            'profile_image' => ['mimes:jpg,png', 'max:2048'],
+            'profile_image' => ['required','mimes:jpg,png', 'max:2048'],
         ];
     }
 }

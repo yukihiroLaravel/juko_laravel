0b842514ddea4a93c172af24e26d8b54a711cef0
kumiko godai <cocoflav.kuminmi127@gmail.com>
2023-12-12 22:19:55 +0900

    ロジックの作成

--- app/Http/Controllers/Api/Manager/ChapterController.php
+++ app/Http/Controllers/Api/Manager/ChapterController.php
@@ -2,8 +2,17 @@
 
 namespace App\Http\Controllers\Api\Manager;
 
+use App\Model\Instructor;
+use App\Model\Course;
+use App\Model\Chapter;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Auth;
 
 class ChapterController extends Controller
 {
@@ -11,8 +20,29 @@ class ChapterController extends Controller
       * マネージャ配下のチャプター削除API
       *
       */
-    public function delete()
+    public function delete(Request $request)
     {
-        return response()->json([]);
+        $instructorId = Auth::guard('instructor')->user()->id;
+        $manager = Instructor::with('managings')->find($instructorId);
+        $instructorIds = $manager->managings->pluck('id')->toArray();
+        $instructorIds[] = $instructorId;
+
+        //自分と配下のインストラクターが持っている講座を取得
+        $course = Course::FindOrFail($request->course_id);
+
+        if (!in_array($course->instructor_id, $instructorIds, true)) {
+            // 自分、または配下の講師の講座のチャプターでなければエラー応答
+            return response()->json([
+                'result'  => false,
+                'message' => "Forbidden, not allowed to delete this chapter.",
+            ], 403);
+        }
+
+        $chapter = Chapter::with('course')->findOrFail($request->chapter_id);
+        
+        $chapter->delete();
+        return response()->json([
+            "result" => true
+        ]);
     }
 }

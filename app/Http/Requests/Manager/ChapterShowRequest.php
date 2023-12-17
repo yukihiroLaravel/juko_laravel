82f0308697d6255db9d7444243299ff50d4ec7fc
jun onodera <o8jmsch@outlook.com>
2023-12-17 21:08:11 +0900

    JKA-581_(ルール追加)_(でら)_フォームリクエスト作成

--- app/Http/Requests/Manager/ChapterShowRequest.php
+++ app/Http/Requests/Manager/ChapterShowRequest.php
@@ -32,8 +32,8 @@ class ChapterShowRequest extends FormRequest
     public function rules()
     {
         return [
-            'course_id' => ['required','integer'],
-            'chapter_id' => ['required','integer'],
+            'course_id' => ['required', 'integer', 'exists:courses,id,deleted_at,NULL'],
+            'chapter_id' => ['required', 'integer', 'exists:chapters,id,deleted_at,NULL'],
         ];
     }
 }

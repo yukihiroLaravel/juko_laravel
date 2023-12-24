0ca68487c1225a439a0b2182a36e7c7848dbfa0f
toshiki kawauchi <toshiki242zwo@gmail.com>
2023-12-24 22:54:29 +0900

    フォームリクエスト作成　修正

--- app/Http/Requests/Instructor/AttendanceDeleteRequest.php
+++ app/Http/Requests/Instructor/AttendanceDeleteRequest.php
@@ -22,9 +22,9 @@ class AttendanceDeleteRequest extends FormRequest
      * @return array
      */
     public function rules()
-    {
+    {        
         return [
-            'attendance_id' => ['required', 'integer'],
+            'attendance_id' => ['required', 'integer', 'exists:attendances,id,deleted_at,NULL'],
         ];
     }
 

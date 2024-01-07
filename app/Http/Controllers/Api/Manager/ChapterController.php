f3a6be0ad71d4cbc20a3851870add7ef5d7bc588
kumiko godai <cocoflav.kuminmi127@gmail.com>
2024-01-07 22:56:12 +0900

    jka-647 try~catchを追加

--- app/Http/Controllers/Api/Manager/ChapterController.php
+++ app/Http/Controllers/Api/Manager/ChapterController.php
@@ -22,37 +22,45 @@ class ChapterController extends Controller
      */
     public function store(ChapterStoreRequest $request)
     {
-        $instructorId = Auth::guard('instructor')->user()->id;
-        // 配下の講師情報を取得
-        $manager = Instructor::with('managings')->find($instructorId);
-        $instructorIds = $manager->managings->pluck('id')->toArray();
-        $instructorIds[] = $instructorId;
+        try {
+            $instructorId = Auth::guard('instructor')->user()->id;
+            // 配下の講師情報を取得
+            $manager = Instructor::with('managings')->find($instructorId);
+            $instructorIds = $manager->managings->pluck('id')->toArray();
+            $instructorIds[] = $instructorId;
 
-        $course = Course::FindOrFail($request->course_id);
+            $course = Course::FindOrFail($request->course_id);
 
-        if (!in_array($course->instructor_id, $instructorIds, true)) {
-            // 自分、または配下の講師の講座でなければエラー応答
+            if (!in_array($course->instructor_id, $instructorIds, true)) {
+                // 自分、または配下の講師の講座でなければエラー応答
+                return response()->json([
+                    'result'  => false,
+                    'message' => "Forbidden, not allowed to create new chapter.",
+                ], 403);
+            }
+
+            $order =  $course->chapters->count();
+            $newOrder = $order + 1;
+            Chapter::create([
+                'course_id' => $request->course_id,
+                'title' => $request->input('title'),
+                'order' => $newOrder,
+                'status' => Chapter::STATUS_PUBLIC,
+            ]);
+
             return response()->json([
-                'result'  => false,
-                'message' => "Forbidden, not allowed to create new chapter.",
-            ], 403);
-        }
-
-        $order =  $course->chapters->count();
-        $newOrder = $order + 1;
-        Chapter::create([
-            'course_id' => $request->course_id,
-            'title' => $request->input('title'),
-            'order' => $newOrder,
-            'status' => Chapter::STATUS_PUBLIC,
-        ]);
-
-        return response()->json([
-            'result' => true,
-        ]);
+                'result' => true,
+            ]);
+            
+            } catch (Exception $e) {
+                Log::error($e);
+                return response()->json([
+                    'result' => false
+                ], 500);
+            }
     }
-    
-    /**
+
+     /**
      * チャプター詳細情報を取得
      *
      * @param ChapterShowRequest $request
@@ -70,15 +78,14 @@ class ChapterController extends Controller
         $chapter = Chapter::with(['lessons','course'])->findOrFail($request->chapter_id);
         // 自身もしくは配下のinstructorでない場合はエラー応答
         if (!in_array($chapter->course->instructor_id, $instructorIds, true)) {
-            return response()->json([
+             return response()->json([
                 'result' => false,
                 'message' => "Forbidden, not allowed to edit this course.",
             ], 403);
         }
-
         return new ChapterShowResource($chapter);
     }
-
+    
     /**
      * マネージャー配下のチャプター更新API
      *

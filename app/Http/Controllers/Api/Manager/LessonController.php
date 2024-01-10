a1263f898b5b40ee4a3d2ec5d33c3a989d1c1a52
kumiko godai <cocoflav.kuminmi127@gmail.com>
2024-01-10 23:22:15 +0900

    #jka-658 空の配列を返すルーターとコントローラーの作成

---
+++ app/Http/Controllers/Api/Manager/LessonController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\Manager;
+
+use App\Http\Controllers\Controller;
+
+class LessonController extends Controller
+{
+    /**
+     * マネージャ配下のレッスン更新API
+     *
+     * 
+     * 
+     */
+    public function update(){
+        return response()->json([]);
+    }
+}

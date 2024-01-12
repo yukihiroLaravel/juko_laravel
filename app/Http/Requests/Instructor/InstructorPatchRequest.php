215930cb0666f58eed10fac5f41510a87cca97a3
Kou <hikkappi@yahoo.co.jp>
2024-01-12 09:03:52 +0900

    Remove unused import statement in InstructorPatchRequest.php

--- app/Http/Requests/Instructor/InstructorPatchRequest.php
+++ app/Http/Requests/Instructor/InstructorPatchRequest.php
@@ -3,7 +3,6 @@
 namespace App\Http\Requests\Instructor;
 
 use App\Rules\InstructorUniqueEmailRule;
-use App\Rules\InstructorTypeRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
 

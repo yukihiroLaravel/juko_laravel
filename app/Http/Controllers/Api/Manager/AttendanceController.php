000a7801ff28766a812e22e2fe666d80181ceddb
harunobu murai <mu19910624rai@gmail.com>
2024-01-15 02:38:53 +0900

    JKA-666 空の配列を返すルータとコントローラの作成

---
+++ app/Http/Controllers/Api/Manager/AttendanceController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\Manager;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AttendanceController extends Controller
+{
+    public function store()
+    {
+        return response()->json([]);
+    }
+}

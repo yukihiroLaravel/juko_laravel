bd4b9711df5cb698f4a8a4d8d6bd088377761634
tatsuhiro hayashi <t.hayashi4218538@gmail.com>
2024-01-31 20:36:57 +0900

    空の配列を返すルーターとコントローラーの作成

---
+++ app/Http/Controllers/Api/Manager/InstructorController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\Manager;
+
+use App\Http\Controllers\Controller;
+
+class InstructorController extends Controller
+{
+    public function show(){
+        return response()->json([]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -176,6 +176,10 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
                 Route::prefix('attendance')->group(function () {
                     Route::post('/', 'Api\Manager\AttendanceController@store');
                 });
+                // マネージャー-生徒
+                Route::prefix('student')->group(function(){
+                    Route::get('{student_id}', 'Api\Manager\InstructorController@show');
+                });
             });
         });
     });

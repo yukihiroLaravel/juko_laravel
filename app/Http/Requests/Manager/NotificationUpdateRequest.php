4b8e283d14f305cc0dd583d00bf5a7d5ca78985d
hatschi2023 <hatschi.sachi@gmail.com>
2024-02-15 22:54:05 +0900

    修正2_お知らせテーブルのレコード有無チェック

--- app/Http/Requests/Manager/NotificationUpdateRequest.php
+++ app/Http/Requests/Manager/NotificationUpdateRequest.php
@@ -3,11 +3,8 @@
 namespace App\Http\Requests\Manager;
 
 use App\Rules\NotificationUpdateStatusRule;
-use App\Model\Notification;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Support\Facades\Log;
+
 class NotificationUpdateRequest extends FormRequest
 {
     /**
@@ -33,39 +30,13 @@ class NotificationUpdateRequest extends FormRequest
      */
     public function rules()
     {
-        //対象のデータがない（論理削除を含む）場合はエラーを返す
-        try {
-            // Log::info('Notification ID: ' . $this->route('notification_id'));
-            $notification = Notification::findOrFail($this->route('notification_id'));
-
-            //対象のデータがある→論理削除されているかチェック
-            if ($notification->trashed()) {
-                if ($notification->deleted_at === null) {
-                    // 論理削除されている→エラーで返す
-                    return response()->json([
-                        'result' => false,
-                        'message' => 'Notification is soft deleted.'
-                    ], 403);
-                }
-            }
-
-            return [
-                'notification_id' => ['required', 'integer'],
-                'type'            => ['required', new NotificationUpdateStatusRule()],
-                'start_date'      => ['required', 'date_format:Y-m-d H:i:s'],
-                'end_date'        => ['required', 'date_format:Y-m-d H:i:s', 'after:start_date'],
-                'title'           => ['required', 'string', 'max:50'],
-                'content'         => ['required', 'string', 'max:500'],
-            ];
-
-        //対象のデータがない→エラーで返す
-        } catch (Exception $e) {
-            Log::error($e);
-            return response()->json([
-                'result' => false,
-                // 'message' => "This Notification does not exist."
-            ], 500);
-        }
-
+        return [
+            'notification_id' => ['required', 'integer','exists:notifications,id,deleted_at,NULL'],
+            'type'            => ['required', new NotificationUpdateStatusRule()],
+            'start_date'      => ['required', 'date_format:Y-m-d H:i:s'],
+            'end_date'        => ['required', 'date_format:Y-m-d H:i:s', 'after:start_date'],
+            'title'           => ['required', 'string', 'max:50'],
+            'content'         => ['required', 'string', 'max:500'],
+        ];
     }
 }

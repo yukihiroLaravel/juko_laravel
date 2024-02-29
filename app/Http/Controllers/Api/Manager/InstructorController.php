40e9dfe1f2c77de85156e3be6cc66ad23018fde7
kazufumi katsuyama <kattsunt970138@gmail.com>
2024-02-29 11:39:44 +0900

    JKA-737自分と配下の講師の講師情報取得(編集)フォームリクエスト作成(かずふみ)

--- app/Http/Controllers/Api/Manager/InstructorController.php
+++ app/Http/Controllers/Api/Manager/InstructorController.php
@@ -3,36 +3,43 @@
 namespace App\Http\Controllers\Api\Manager;
 
 use App\Http\Controllers\Controller;
-use App\Model\Instructor;
-use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\Manager\InstructorEditResource;
+use App\Http\Requests\Manager\InstructorEditRequest;
+use App\Model\Instructor;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class InstructorController extends Controller
 {
     /**
      * 講師情報編集API
-     *
+     * Auth::guard('instructor')
      * @param InstructorEditRequest $request
      * @return InstructorEditResource|\Illuminate\Http\JsonResponse
      */
-    public function edit($instructor_id)
+    public function edit(InstructorEditRequest $request)
     {
+        //フォームリクエストから講師情報の取得
+        $requestInstructor = $request->instructor_id;
+        
+        // 現在のユーザーを取得（講師の場合）
         $instructorId = Auth::guard('instructor')->user()->id;
+
         // 配下の講師情報を取得
         $manager = Instructor::with('managings')->findOrFail($instructorId);
         $instructorIds = $manager->managings->pluck('id')->toArray();
         $instructorIds[] = $instructorId;
 
         //指定した講師IDが自分と配下の講師IDと一致しない場合は許可しない
-        if (!in_array((int)$instructor_id, $instructorIds, true)) {
-            // 自分、または配下の講師でなければエラー応答
+        if (!in_array((int)$requestInstructor, $instructorIds, true)) {
             return response()->json([
                 'result'  => false,
                 'message' => "Forbidden, not allowed to edit this course.",
             ], 403);
         }
-
-        $instructor = Instructor::findOrFail($instructor_id);
+        
+        $instructor = Instructor::findOrFail($requestInstructor);
         return new InstructorEditResource($instructor);
     }
+
 }
---
+++ app/Http/Requests/Manager/InstructorEditRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests\Manager;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class InstructorEditRequest extends FormRequest
+{
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'instructor_id' => $this->route('instructor_id'),
+        ]);
+    }
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'instructor_id' => ['required', 'integer'],
+        ];
+    }
+}

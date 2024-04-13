85e2720315056a0cb219212e27192d3056c55af7
Kou <hikkappi@yahoo.co.jp>
2024-04-13 11:10:21 +0900

    Add: l5swaggerの導入。

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,12 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *   title="Laravel API",
+ *   version="1.0.0",
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests;

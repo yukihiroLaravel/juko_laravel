77cd5a526664a943aade5d4acee429e47b98c417
seiji kobayashi <se06.program@gmail.com>
2024-04-15 00:12:26 +0900

    JKA-779 講師側受講状況API ロジックの作成(小林）⑨

--- app/Model/Attendance.php
+++ app/Model/Attendance.php
@@ -5,7 +5,6 @@ namespace App\Model;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 
 /**

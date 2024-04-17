f38a77a3d2ea051b415f1c26da552b6be3de2ca5
yuyakondo333 <3939sereson@gmail.com>
2024-04-17 23:04:21 +0900

    空の配列を返すルーティングとコントローラー

---
+++ app/Http/Controllers/Api/Student/NotificationController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Student;
+
+use App\Http\Controllers\Controller;
+
+class NotificationController extends Controller
+{
+    public function index()
+    {
+        return response()->json();
+    }
+}

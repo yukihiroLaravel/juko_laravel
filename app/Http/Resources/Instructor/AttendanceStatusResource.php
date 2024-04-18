91bae72229461484611eb1eceb185ec1c9e74365
seiji kobayashi <se06.program@gmail.com>
2024-04-18 19:11:48 +0900

    JKA-779 講師側受講状況API レスポンス整形のリソースファイル作成（小林） chapter_idとlesson_idの紐づけ及びlesson進捗の割合表記

--- app/Http/Resources/Instructor/AttendanceStatusResource.php
+++ app/Http/Resources/Instructor/AttendanceStatusResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Instructor;
 
+use App\Model\LessonAttendance;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AttendanceStatusResource extends JsonResource
@@ -16,23 +17,39 @@ class AttendanceStatusResource extends JsonResource
     {
         $attendance = $this->attendance;
 
-        return [
-            'attendance_id' => $attendance->id,
+        if ($attendance !== null && is_object($attendance)) {
+            return [
+                'attendance_id' => $attendance->id,
                 'progress' => $attendance->progress,
                 'course' => [
                     'course_id' => $attendance->course->id,
                     'title' => $attendance->course->title,
                     'status' => $attendance->course->status,
                     'image' => $attendance->course->image,
-                    'chapters' => $attendance->course->chapters->map(function (Chapter $chapter) {
+                    'chapters' => $attendance->course->chapters->map(function ($chapter) use ($attendance) {
                         return [
                             'chapter_id' => $chapter->id,
                             'title' => $chapter->title,
                             'status' => $chapter->status,
                             'progress' => $attendance->progress,
+                            'lessons' => $chapter->lessons->map(function ($lesson) use ($attendance) {
+
+                                $lessonAttendance = LessonAttendance::where('lesson_id', $lesson->id)
+                                                                    ->where('attendance_id', $attendance->id)
+                                                                    ->first();
+                                return [
+                                    'lesson_id' => $lesson->id,
+                                    'title' => $lesson->title,
+                                    'status' => $lessonAttendance ? $lessonAttendance->status : null,
+                                    'progress' => $lessonAttendance && $lessonAttendance->status === LessonAttendance::STATUS_COMPLETED_ATTENDANCE ? 100 : 0,
+                                ];
+                            }),
                         ];
                     }),
                 ],
-        ];
+            ];
+        } else {
+            return [];
+        }
     }
 }

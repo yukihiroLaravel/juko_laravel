33551d1442738a7519c6b494df204cb1bd91024f
abeai0 <115936489+abeai0@users.noreply.github.com>
2024-05-01 23:38:03 +0900

    コース一覧取得

---
+++ app/Http/Resources/Manager/InstructorCourseIndexResource.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Resources\Manager;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class InstructorCourseIndexResource extends JsonResource
+{
+    /** @var LengthAwarePaginator */
+    public $resource;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        $courses = $this->resource;
+        return [
+            "courses" => $this->mapCourses($courses),
+            "pagination" => [
+                "page" => $courses->currentPage(),
+                "total" => $courses->total()
+            ]
+        ];
+    }
+
+    /**
+     * @param Collection<\App\Model\Notification> $notifications
+     * @return array
+     */
+    private function mapCourses($courses)
+    {
+        return $courses->map(function ($course) {
+            return [
+                "course_id" => $course->id,
+                "title" => $course->title,
+                "status" => $course->status
+            ];
+        });
+    }
+}

9a13ddd67f1d85957b1835f0d8e3bb005f672340
seiji kobayashi <se06.program@gmail.com>
2024-05-09 09:02:36 +0900

    JKA-838 講師側お知らせ削除API ロジックの作成

--- app/Http/Controllers/Api/Instructor/NotificationController.php
+++ app/Http/Controllers/Api/Instructor/NotificationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Instructor;
 
 use App\Model\Notification;
+use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
@@ -98,5 +99,23 @@ class NotificationController extends Controller
         return response()->json([
             'result' => true,
         ]);
+    }
+
+    /**
+    * お知らせ全削除
+    *
+    * @param Request $request
+    * @return JsonResponse
+    */
+    public function delete(Request $request): JsonResponse
+    {
+    $notificationIds = $request->input('notifications', []);
+
+    $user = Auth::guard('instructor')->user();
+    Notification::whereIn('id', $notificationIds)
+                ->where('instructor_id', $user->id)
+                ->delete();
+
+    return response()->json(['result' => true]);
     }
 }

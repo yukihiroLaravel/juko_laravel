4a4f9e17c9779d9d6d8aeb65925b6d54cabb1d8a
Sorato Ishi <sky.movie.1012@gmail.com>
2024-05-15 21:10:31 +0900

    マネージャー_受講情報を削除するAPI作成

--- app/Http/Controllers/Api/Manager/AttendanceController.php
+++ app/Http/Controllers/Api/Manager/AttendanceController.php
@@ -125,8 +125,7 @@ class AttendanceController extends Controller
             }
 
             // 受講状況を削除
-            $attendance = Attendance::findOrFail($attendanceId);
-            $attendance->delete();
+            $attendance = Attendance::findOrFail($attendanceId)->delete();
 
             DB::commit();
             return response()->json([

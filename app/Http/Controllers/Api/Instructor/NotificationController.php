d30f8c1fe5fe1bc76e02d7d6c6f7b8b855d01988
seiji kobayashi <se06.program@gmail.com>
2024-05-15 21:35:44 +0900

    JKA-838 講師側お知らせ削除API ロジック作成 viewed_once_notificationsのレコードを削除+トランザクションを導入 認証者を講師に変更

--- app/Http/Controllers/Api/Instructor/NotificationController.php
+++ app/Http/Controllers/Api/Instructor/NotificationController.php
@@ -118,13 +118,13 @@ class NotificationController extends Controller
         /** @var Collection $notifications */
         $notifications = Notification::whereIn('id', $notificationIds)->get();
 
-        // 認証者と一致しないお知らせが含まれている場合はエラー
+        // 講師と一致しないお知らせが含まれている場合はエラー
         if (
             $notifications->contains(function (Notification $notification) use ($instructor) {
                 return $notification->instructor_id !== $instructor->id;
             })
         ) {
-            // 認証者と一致しないお知らせが含まれている場合はエラー
+            // 講師と一致しないお知らせが含まれている場合はエラー
             return response()->json([
                 'result' => false,
                 'message' => 'Forbidden.',

827ffd3bd4d200bd320bb415df6510ddaeb00c96
koudai onodera <iamlucky6406@gmail.com>
2024-05-31 16:55:58 +0900

    #jka-886 マネージャー側　本日のレッスン　空の配列を返す

--- routes/api.php
+++ routes/api.php
@@ -188,6 +188,12 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
                                 });
                             });
                         });
+                        //マネージャー-講座-受講
+                        Route::prefix('attendance')->group(function () {
+                            Route::prefix('status')->group(function () {
+                                Route::get('today', 'Api\Manager\AttendanceController@showStatusToday');
+                            });
+                        });
                     });
                 });
                 // マネージャー-受講

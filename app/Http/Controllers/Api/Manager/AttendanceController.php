57a47df056d9fc3fb4df0b00764f9c87d4faaecd
aya yamamoto <trfl.ack3@gmail.com>
2024-07-05 23:28:07 +0900

    JKA-934 今月完了したレッスン数を取得し、APIから返却する

--- app/Http/Controllers/Api/Manager/AttendanceController.php
+++ app/Http/Controllers/Api/Manager/AttendanceController.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 use App\Http\Requests\Manager\AttendanceStoreRequest;
 use App\Http\Requests\Manager\AttendanceDeleteRequest;
+use App\Http\Requests\Instructor\AttendanceShowRequest;
+use App\Http\Requests\Instructor\AttendanceShowThisMonthRequest;
 
 class AttendanceController extends Controller
 {
@@ -146,8 +149,26 @@ class AttendanceController extends Controller
     *
     *
     */
-   public function showStatusThisMonth()
+    
+    /**
+     * 講座受講状況-今月
+     *
+     * @param AttendanceShowThisMonthRequest $request
+     * @return JsonResponse
+     */
+   public function showStatusThisMonth(Request $request): JsonResponse
    {
-       return response()->json([]);
+        $attendances = Attendance::with('lessonAttendances.lesson.chapter.course')->where('course_id', $request->course_id)->get();
+    
+        // 今月完了したレッスンの個数を取得
+        $completedLessonsCount = $attendances->flatMap(function (Attendance $attendance) {
+            $compleatedLessonAttendances = $attendance->lessonAttendances->filter(function (LessonAttendance $lessonAttendance) {
+                return $lessonAttendance->status === LessonAttendance::STATUS_COMPLETED_ATTENDANCE && $lessonAttendance->updated_at->isCurrentMonth();
+        });
+        return $compleatedLessonAttendances;
+    })->count();
+       return response()->json([
+        'completed_lessons_count' => $completedLessonsCount,
+       ]);
    }
 }

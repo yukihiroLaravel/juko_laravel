f9e6e94deee64134579f32cc1116213be87156eb
yuta tokeshi <fleatoke@gmail.com>
2024-07-19 23:02:42 +0900

    resolve confrict2

--- app/Http/Controllers/Api/Manager/AttendanceController.php
+++ app/Http/Controllers/Api/Manager/AttendanceController.php
@@ -146,7 +146,8 @@ class AttendanceController extends Controller
     /**
      * 本日のレッスン・チャプター完了数の取得API
      *
-     *
+     * @param AttendanceShowRequest $request
+     * @return \Illuminate\Http\JsonResponse
      */
     public function showStatusToday(AttendanceShowRequest $request): JsonResponse
     {

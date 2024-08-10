9707a472d8d89c29f5b890fd56e8deaba5b9f46f
yuta tokeshi <fleatoke@gmail.com>
2024-08-10 21:51:47 +0900

    merge_attendances

--- app/Http/Controllers/Api/Manager/LessonController.php
+++ app/Http/Controllers/Api/Manager/LessonController.php
@@ -361,13 +361,11 @@ class LessonController extends Controller
 
         //レッスン情報を取得
         /** @var Lesson $lesson */
-        $lesson = Lesson::with('chapter.course')->whereIn('id', $lessonIds)->get();
-        //アテンダンス情報を取得
-        $attendedLessons = LessonAttendance::whereIn('lesson_id', $lessonIds)->pluck('lesson_id')->toArray();
+        $lesson = Lesson::with('chapter.course', 'attendances')->whereIn('id', $lessonIds)->get();
         //認可チェック
         try {
             //レッスンデータの認可チェック
-            $lesson->each(function (Lesson $lesson) use ($instructorIds, $chapterId, $courseId, $attendedLessons) {
+            $lesson->each(function (Lesson $lesson) use ($instructorIds, $chapterId, $courseId) {
                 //自身もしくは配下のinstructorの講座・チャプターに紐づくレッスンでない場合は許可しない
                 if (!in_array($lesson->chapter->course->instructor_id, $instructorIds, true)) {
                     throw new ValidationErrorException('Invalid instructor_id.');
@@ -381,7 +379,7 @@ class LessonController extends Controller
                     throw new ValidationErrorException('Invalid chapter_id.');
                 }
                 //受講情報が登録されている場合は許可しない
-                if (in_array($lesson->id, $attendedLessons)) {
+                if ($lesson->attendances->isNotEmpty()) {
                     throw new ValidationErrorException('This lesson has attendance.');
                 }
             });

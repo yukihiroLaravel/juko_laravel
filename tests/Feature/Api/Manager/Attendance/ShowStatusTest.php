233353997219f0c5c994263fa67d8c11e0abdf7d
Kou <hikkappi@yahoo.co.jp>
2025-01-18 00:16:54 +0900

    Add : 正常系のテストを追加。

---
+++ tests/Feature/Api/Manager/Attendance/ShowStatusTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\Api\Manager\Attendance;
+
+use App\Model\Instructor;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ShowStatusTest extends TestCase
+{
+    use RefreshDatabase;
+
+    // setup
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+    }
+
+    public function test_当日の出席状況を取得_成功(): void
+    {
+        // arrange
+        $instructor = Instructor::find(1);
+        $this->actingAs($instructor, 'instructor');
+
+        // act
+        $response = $this->getJson('/api/v1/manager/course/1/attendance/status/today');
+
+        // assert
+        $response->assertStatus(200);
+    }
+
+    public function test_今月の出席状況を取得_成功(): void
+    {
+        // arrange
+        $instructor = Instructor::find(1);
+        $this->actingAs($instructor, 'instructor');
+
+        // act
+        $response = $this->getJson('/api/v1/manager/course/1/attendance/status/month');
+
+        // assert
+        $response->assertStatus(200);
+    }
+}

cc93ff9555680ea79a8330504cb7c8ff59d1610b
masafumi oohata <masafumi.oohata@gmail.com>
2025-01-24 00:38:58 +0900

    Instructor/StudentControllerの未対応部分にthrow new AuthorizationExceptionとthrow の適用(コンフリクト修正2)

--- app/Http/Controllers/Api/Student/StudentController.php
+++ app/Http/Controllers/Api/Student/StudentController.php
@@ -260,33 +260,8 @@ class StudentController extends Controller
                 'message' => 'Authorization success.',
             ]);
 
-
-        } catch (ModelNotFoundException $e) {
-            return response()->json([
-                'result' => false,
-                'message' => 'Not Found data to match token.',
-            ], 404);        
-        } catch (ExpiredAuthorizationCodeException $e) {
-            $studentAuth->delete();
-
-            return response()->json([
-                'result' => false,
-                'message' => 'Expired authorization period.',
-            ], 406);
-        } catch (TryCountOverAuthorizationCodeException $e) {
-            $studentAuth->delete();
-
-            return response()->json([
-                'result' => false,
-                'message' => 'Not match authorization code three times.',
-            ], 400);
         } catch (Exception $e) {
-            DB::rollBack();
             Log::error($e);
-            return response()->json([
-                'result' => false,
-            ], 500);
-
         }
     }
 }

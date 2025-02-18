56de6356832d63896d7e72d804f5e6a9b2e785fb
NeitoKomiyama <neito.komi11@gmail.com>
2025-02-18 16:10:21 +0900

    jka-1148:指摘事項の修正

--- app/Model/Tag.php
+++ app/Model/Tag.php
@@ -4,7 +4,6 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Tag extends Model
 {

cca828f25c10b748c17c5ab40a2238174be23842
Kou <hikkappi@yahoo.co.jp>
2025-02-19 22:13:32 +0900

    Import忘れを修正。

--- app/Model/Course.php
+++ app/Model/Course.php
@@ -4,6 +4,7 @@ namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 

6450ce93278908c1932ed30ad6a0ebe5a6b61f9a
natsumi kawahara <m.f.natsu.k75@gmail.com>
2025-02-19 10:40:21 +0900

    【訂正】指摘箇所修正②

--- app/Http/Resources/Instructor/CourseIndexResource.php
+++ app/Http/Resources/Instructor/CourseIndexResource.php
@@ -24,8 +24,6 @@ class CourseIndexResource extends JsonResource
             'title' => $this->resource->title,
             'status' => $this->resource->status,
             'has_active_students' => (bool) $this->resource->has_active_students,
-            'per_page' => ['integer', 'min:1', 'max:100'],
-            'page' => ['integer', 'min:1'],
         ];
     }
 }

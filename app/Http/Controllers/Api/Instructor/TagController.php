1eeef39ab81072e96b4b0f7e1e3531a6a1176ddf
NeitoKomiyama <neito.komi11@gmail.com>
2025-02-20 15:59:42 +0900

    jka-1162:空の配列を返すルーティングとコントローラーの作成

---
+++ app/Http/Controllers/Api/Instructor/TagController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api\Instructor;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * @tags Instructor-Tag
+ */
+class TagController extends Controller
+{
+    /**
+     * 講座分類更新API
+     */
+    public function update()
+    {
+        return response()->json([]);
+    }
+}

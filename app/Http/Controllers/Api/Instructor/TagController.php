b62ecf038721567505f4d4e334e897a39609bf97
NeitoKomiyama <neito.komi11@gmail.com>
2025-02-21 13:18:13 +0900

    jka-1163：ロジックの作成

--- app/Http/Controllers/Api/Instructor/TagController.php
+++ app/Http/Controllers/Api/Instructor/TagController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers\Api\Instructor;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Model\Instructor;
+use App\Model\Tag;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * @tags Instructor-Tag
@@ -12,8 +17,21 @@ class TagController extends Controller
     /**
      * 講座分類更新API
      */
-    public function update()
+    public function put(Request $request)
     {
-        return response()->json([]);
+        $user = Instructor::find(Auth::guard('instructor')->user()->id);
+        $tag = Tag::FindOrFail($request->tag_id);
+
+        if ($user->id !== $tag->instructor_id){
+            throw new AuthorizationException('Invalid instructor_id.');
+        }
+
+        $tag->update([
+            'content' => $request->content,
+        ]);
+
+        return response()->json([
+            'result' => true,
+        ]);
     }
 }

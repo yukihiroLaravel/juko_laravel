76e192c460ef0057e973e6a956f8a88a3c38b288
Kou <hikkappi@yahoo.co.jp>
2025-02-21 17:55:16 +0900

    Add : 自動テストを追加。

--- app/Http/Controllers/Api/Instructor/TagController.php
+++ app/Http/Controllers/Api/Instructor/TagController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Model\Instructor;
 use App\Model\Tag;
 use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,13 +18,13 @@ class TagController extends Controller
     /**
      * 講座分類更新API
      */
-    public function put(Request $request)
+    public function put(Request $request): JsonResponse
     {
         $user = Instructor::find(Auth::guard('instructor')->user()->id);
         $tag = Tag::FindOrFail($request->tag_id);
 
         if ($user->id !== $tag->instructor_id) {
-            throw new AuthorizationException('Invalid instructor_id.');
+            throw new AuthorizationException('Forbidden, invalid instructor.');
         }
 
         $tag->update([

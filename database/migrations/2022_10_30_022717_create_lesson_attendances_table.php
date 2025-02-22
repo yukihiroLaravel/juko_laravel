3ed90cfdb0eb7849c17b78cbe6056e63c7331bac
Kou <hikkappi@yahoo.co.jp>
2025-02-23 06:57:12 +0900

    外部キーの追加。

--- database/migrations/2022_10_30_022717_create_lesson_attendances_table.php
+++ database/migrations/2022_10_30_022717_create_lesson_attendances_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Model\Attendance;
+use App\Model\Lesson;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,14 +17,12 @@ class CreateLessonAttendancesTable extends Migration
     {
         Schema::create('lesson_attendances', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->bigInteger('lesson_id')->unsigned()->comment('レッスンID');
-            $table->bigInteger('attendance_id')->unsigned()->comment('受講ID');
+            $table->foreignIdFor(Lesson::class)->constrained()->comment('レッスンID');
+            $table->foreignIdFor(Attendance::class)->constrained()->comment('受講ID');
             $table->string('status', 30)->comment('レッスン受講状態');
             $table->dateTime('created_at');
             $table->dateTime('updated_at');
             $table->softDeletes();
-            $table->foreign('lesson_id')->references('id')->on('lessons'); //外部キー制約
-            $table->foreign('attendance_id')->references('id')->on('attendances'); //外部キー制約
         });
     }
 

aaafb88d2134c7d62b4a2bc07e554c2efdb680a5
shoko nishiyama <superawesome.kujira@gmail.com>
2025-02-22 10:12:14 +0900

    jka-1166(親1154)ロジックの作成(西山しょうこ)

--- app/Http/Controllers/Api/Instructor/TagController.php
+++ app/Http/Controllers/Api/Instructor/TagController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers\Api\Instructor;
 
 use App\Http\Controllers\Controller;
+use App\Model\Tag;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 /**
  * @tags Instructor-Tag
@@ -12,8 +17,19 @@ class TagController extends Controller
     /**
      * 講座分類（タグ）登録API
      */
-    public function store()
+    public function store(Request $request): JsonResponse
     {
-        return response()->json([]);
+        $instructorId = Auth::guard('instructor')->user()->id;
+
+        Tag::create([
+            'instructor_id' => $instructorId,
+            'content' => $request->content,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        
+        return response()->json([
+            'result' => true,
+        ]);
     }
 }

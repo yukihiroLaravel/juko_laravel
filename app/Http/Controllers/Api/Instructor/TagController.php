19e568f0efa0a2e961f015f370565d1d0fe22065
yuki tonomoto <yukinko_glay@yahoo.co.jp>
2025-02-23 14:23:53 +0900

    ロジックの作成(yuki)

--- app/Http/Controllers/Api/Instructor/TagController.php
+++ app/Http/Controllers/Api/Instructor/TagController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Api\Instructor;
 
 use App\Http\Controllers\Controller;
+use App\Model\Course;
+use App\Model\Tag;
+use Illuminate\Support\Facades\Auth;
 
 class TagController extends Controller
 {
@@ -11,6 +14,15 @@ class TagController extends Controller
      */
     public function index()
     {
-        return response()->json([]);
+        // ログインしている講師
+        $instructorId = Auth::guard('instructor')->user()->id;
+
+        $tags = Tag::where('instructor_id', $instructorId)->select('id', 'content')->get();
+        $courses = Course::where('instructor_id', $instructorId)->select('id', 'title', 'image', 'status')->get();
+
+        return response()->json([
+            'tags' => $tags,
+            'courses' => $courses
+        ]);
     }
 }

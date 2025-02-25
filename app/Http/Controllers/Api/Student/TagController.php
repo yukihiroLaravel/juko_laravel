fa73f98d1a50150996faffe6562a068b9ee9e638
takeuchi <panicdepanic@gmail.com>
2025-02-26 08:09:27 +0900

    jka-1169(親:1149)空の配列を返すルーターとコントローラの作成 竹内

---
+++ app/Http/Controllers/Api/Student/TagController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TagController extends Controller
+{
+    public function get() {
+        return response()->json([]);
+    }
+}

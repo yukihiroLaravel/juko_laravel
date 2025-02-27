37af1cdf2a5cd00375f3654b89658e0225ad5693
natsumi kawahara <m.f.natsu.k75@gmail.com>
2025-02-27 09:50:49 +0900

    一旦コミット

--- app/Http/Controllers/Api/Manager/TagController.php
+++ app/Http/Controllers/Api/Manager/TagController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers\Api\Manager;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Model\Instructor;
+use App\Model\Tag;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
 
 /**
  * @tags Manager-Tag
@@ -12,8 +18,24 @@ class TagController extends Controller
     /**
      * 講座分類更新API
      */
-    public function update()
+    public function put(Request $request): JsonResponse
     {
-        return response()->json([]);
+
+        $user = Instructor::find(Auth::guard('manager')->user()->id);
+        $tag = Tag::findOrFail($request->tag_id);
+
+        // マネージャー本人または配下の講師が作成したタグのみ更新可能
+        $subordinateIds = $user->getSubordinateIds();
+        if ($user->id !== $tag->manager_id && !in_array($tag->manager_id, $subordinateIds)) {
+            throw new AuthorizationException('Invalid manager.');
+        }
+
+        $tag->update([
+            'content' => $request->content,
+        ]);
+
+        return response()->json([
+            'result' => true,
+        ]);
     }
 }

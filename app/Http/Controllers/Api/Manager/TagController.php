5227c5661a3aaef06e3af4768449a5292826076d
natsumi kawahara <m.f.natsu.k75@gmail.com>
2025-02-28 23:08:51 +0900

    指摘箇所修正

--- app/Http/Controllers/Api/Manager/TagController.php
+++ app/Http/Controllers/Api/Manager/TagController.php
@@ -20,12 +20,6 @@ class TagController extends Controller
      */
     public function put(Request $request): JsonResponse
     {
-        // バリデーション
-        $validated = $request->validate([
-            'tag_id' => 'required|integer|exists:tags,id',
-            'content' => 'required|string',
-        ]);
-
         // マネージャーが管理する講師IDを取得
         $instructorId = Auth::guard('instructor')->user()->id;
 
@@ -35,16 +29,16 @@ class TagController extends Controller
         $instructorIds[] = $manager->id; // 自身のIDも追加
 
         // タグの取得
-        $tag = Tag::findOrFail($validated['tag_id']);
+        $tag = Tag::findOrFail($request->tag_id);
 
         // 配下のインストラクターまたは本人が作成したタグのみ更新可能
         if (!in_array($tag->instructor_id, $instructorIds, true)) {
-            throw new AuthorizationException('Unauthorized');
+            throw new AuthorizationException('Forbidden, invalid instructor_id.');
         }
 
         // タグの更新
         $tag->update([
-            'content' => $validated['content'],
+            'content' => $request->content,
         ]);
 
         return response()->json([

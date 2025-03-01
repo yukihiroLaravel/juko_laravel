d284e876bc7815dd138322353def7260e006351b
NeitoKomiyama <neito.komi11@gmail.com>
2025-03-01 09:02:39 +0900

    jka-1174:フォームリクエストの作成

--- app/Http/Controllers/Api/Instructor/TagController.php
+++ app/Http/Controllers/Api/Instructor/TagController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Api\Instructor;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Instructor\Tag\PutRequest;
 use App\Http\Requests\Instructor\Tag\StoreRequest;
+use App\Http\Requests\Instructor\Tag\ShowRequest;
 use App\Http\Resources\Instructor\TagIndexResource;
+use App\Http\Resources\Tag\TagResource;
 use App\Model\Instructor;
 use App\Model\Tag;
 use Illuminate\Auth\Access\AuthorizationException;
@@ -51,7 +53,7 @@ class TagController extends Controller
     /**
      * タグ詳細API
      */
-    public function show(Request $request): JsonResponse
+    public function show(ShowRequest $request)
     {
         $instructorId = Auth::guard('instructor')->user()->id;
 
@@ -61,12 +63,7 @@ class TagController extends Controller
             throw new AuthorizationException('Forbidden, invalid instructor_id.');
         }
 
-        return response()->json([
-            'data' => [
-                'tag_id' => $tag->id,
-                'content' => $tag->content,
-            ],
-        ]);
+        return new TagResource($tag);
     }
 
     /**
---
+++ app/Http/Requests/Instructor/Tag/ShowRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests\Instructor\Tag;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ShowRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'tag_id' => $this->route('tag_id'),
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'tag_id' => ['required', 'integer', 'exists:tags,id'],
+        ];
+    }
+}

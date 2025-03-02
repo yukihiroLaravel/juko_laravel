d3c1390b12fc48f73cfc369dea0bcfa04b2027f5
Shinya-Nakaguchi <snfjkdh@gmail.com>
2025-03-02 23:12:02 +0800

    logic

--- app/Http/Controllers/Api/Manager/TagController.php
+++ app/Http/Controllers/Api/Manager/TagController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers\Api\Manager;
 
-use App\Http\Controllers\Controller;
+use App\Model\Tag;
 use Illuminate\Http\JsonResponse;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Auth\Access\AuthorizationException;
 
 /**
  * @tags Manager-Tag
@@ -13,8 +17,21 @@ class TagController extends Controller
     /**
      * 講座分類詳細API
      */
-    public function show(): JsonResponse
+    public function show(Request $request): JsonResponse
     {
-        return response()->json([]);
+        $instructorId = Auth::guard('instructor')->user()->id;
+
+        $tag = Tag::findOrFail($request->tag_id);
+
+        if ($tag->instructor_id !== $instructorId) {
+            throw new AuthorizationException('Forbidden, invalid instructor_id.');
+        }
+
+        return response()->json([
+            'data' => [
+                'tag_id' => $tag->id,
+                'content' => $tag->content,
+            ],
+        ]);
     }
 }

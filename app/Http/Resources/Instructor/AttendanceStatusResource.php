167f67bfc28db89126f5f5374b8d3a03dc64aceb
natsumi kawahara <m.f.natsu.k75@gmail.com>
2025-03-05 09:50:02 +0900

    講師側 受講生詳細画面の学習状況(受講状況API)に講座分類名を追加

--- app/Http/Resources/Instructor/AttendanceStatusResource.php
+++ app/Http/Resources/Instructor/AttendanceStatusResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources\Instructor;
 use App\Model\Attendance;
 use App\Model\Chapter;
 use Illuminate\Database\Eloquent\Collection;
+use App\Http\Resources\Tag\TagResource;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AttendanceStatusResource extends JsonResource
@@ -28,6 +29,7 @@ class AttendanceStatusResource extends JsonResource
                 'image' => $this->resource->course->image,
                 'chapters' => $this->mapChapters($this->resource->course->chapters),
                 'title' => $this->resource->course->title,
+                'tags' => TagResource::collection($this->resource->course->tags),
             ],
         ];
     }

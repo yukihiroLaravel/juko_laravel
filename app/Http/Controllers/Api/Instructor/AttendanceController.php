94a60f1c4db4c112924db41b644f6906757d0837
masafumi oohata <masafumi.oohata@gmail.com>
2025-03-08 09:03:43 +0900

    講師側受講状況取得APIに講座分類名を追加JKA-1180

--- app/Http/Controllers/Api/Instructor/AttendanceController.php
+++ app/Http/Controllers/Api/Instructor/AttendanceController.php
@@ -88,7 +88,7 @@ class AttendanceController extends Controller
     {
         $instructorId = Auth::guard('instructor')->user()->id;
         $courseId = $request->course_id;
-        $course = Course::findOrFail($courseId);
+        $course = Course::with('tags')->findOrFail($courseId);
 
         if ($course->instructor_id !== $instructorId) {
             // ログインしている講師の講座でない場合はエラーを返す
@@ -104,6 +104,7 @@ class AttendanceController extends Controller
         return new AttendanceShowResource([
             'chapters' => $chapters,
             'studentsCount' => $studentsCount,
+            'course_tags' => $course->tags->pluck('content'),
         ]);
     }
 
--- app/Http/Resources/Instructor/AttendanceShowResource.php
+++ app/Http/Resources/Instructor/AttendanceShowResource.php
@@ -28,6 +28,7 @@ class AttendanceShowResource extends JsonResource
                 ];
             }),
             'students_count' => $this->resource['studentsCount'],
+            'course_tags' => $this->resource['course_tags'],
         ];
     }
 }

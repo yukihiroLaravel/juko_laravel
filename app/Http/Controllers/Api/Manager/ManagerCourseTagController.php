f7a880f76d1d6cedce6d927f905c764ee3901a10
deuro-ki <hero14yki1986@gmail.com>
2025-03-14 02:23:44 +0900

    JKA-1176_空の配列を返すルーターとコントローラの作成

---
+++ app/Http/Controllers/Api/Manager/ManagerCourseTagController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api\Manager;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ManagerCourseTagController extends Controller
+{
+    public function index() {
+        return response()->json([
+            
+        ]);
+    }
+}

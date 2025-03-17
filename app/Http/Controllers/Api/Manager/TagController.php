c5620a35663f83b6b274cb3e2ef879978202a9df
deuro-ki <hero14yki1986@gmail.com>
2025-03-17 16:22:56 +0900

    (えんどう)JKA-1176_空の配列を返すルーターとコントローラの作成(修正4回目)

--- app/Http/Controllers/Api/Manager/TagController.php
+++ app/Http/Controllers/Api/Manager/TagController.php
@@ -16,6 +16,13 @@ use Illuminate\Support\Facades\Auth;
 class TagController extends Controller
 {
     /**
+     * タグ一覧取得API
+     */
+    public function index() {
+        return response()->json([]);
+    }
+
+    /**
      * タグ更新API
      */
     public function put(PutRequest $request): JsonResponse

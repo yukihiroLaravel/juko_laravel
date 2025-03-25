77e7b53116ebcac41ccfcb8e6178373963339121
deuro-ki <hero14yki1986@gmail.com>
2025-03-26 02:27:45 +0900

    (えんどう)JKA-1176_ロジック作成

--- app/Http/Controllers/Api/Manager/TagController.php
+++ app/Http/Controllers/Api/Manager/TagController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\Manager;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Manager\Tag\PutRequest;
+use Illuminate\Http\Request;
 use App\Model\Instructor;
 use App\Model\Tag;
 use Illuminate\Auth\Access\AuthorizationException;
@@ -18,8 +19,37 @@ class TagController extends Controller
     /**
      * タグ一覧取得API
      */
-    public function index()
-    {
+    public function index(Request $request)
+    {   
+        $tagId = $request->query('tag_id');
+
+        // マネージャーが管理する講師IDを取得
+        $instructorId = Auth::guard('instructor')->user()->id;
+
+        /** @var Instructor $manager */
+        $manager = Instructor::with('managings')->find($instructorId);
+
+        // マネージャー(講師)＋マネージャー配下インストラクターのID一覧
+        $instructorIds = $manager->managings->pluck('id')->toArray();
+        $instructorIds[] = $manager->id; // 自身のIDも追加
+
+        if ($tagId) {
+            // タグの取得
+            $tag = Tag::findOrFail($tagId);
+
+            // ログインしているマネージャー(講師)もしくはその配下の講師とtag_idの講師が一致しない
+            if (!in_array($tag->instructor_id, $instructorIds, true)) {
+                throw new AuthorizationException('Forbidden, invalid instructor_id.');
+            }
+        }
+
+        $query = Tag::where('instructor_id', $instructorId)
+            ->when($tagId, function (Builder $query, string $tagId) {
+                $query->whereHas('courses', fn (Builder $query) => $query->where('tags.id', $tagId));
+            })
+            ->with('courses')
+            ->get();
+
         return response()->json([]);
     }
 

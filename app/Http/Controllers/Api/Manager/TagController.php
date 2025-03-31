6420841edd4b7249a8046ff100104549f2816bb9
deuro-ki <hero14yki1986@gmail.com>
2025-04-01 00:31:07 +0900

    (えんどう)JKA-1176(JKA-1202)_フォームリクエストの作成

--- app/Http/Controllers/Api/Manager/TagController.php
+++ app/Http/Controllers/Api/Manager/TagController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Manager;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Manager\Tag\IndexRequest;
 use App\Http\Requests\Manager\Tag\PutRequest;
 use App\Model\Instructor;
 use App\Model\Tag;
@@ -20,7 +21,7 @@ class TagController extends Controller
     /**
      * タグ一覧取得API
      */
-    public function index(Request $request)
+    public function index(IndexRequest $request)
     {
         $tagId = $request->query('tag_id');
 

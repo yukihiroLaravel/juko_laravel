f2f6f449f0623cdd4a8906c70190ae7c4564e368
Kou <hikkappi@yahoo.co.jp>
2025-04-01 21:04:03 +0900

    Fix : タグリソースの参照箇所を修正。

--- app/Http/Resources/Instructor/AttendanceShowResource.php
+++ app/Http/Resources/Instructor/AttendanceShowResource.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Resources\Instructor;
 
-use App\Http\Resources\Tag\TagResource;
+use App\Http\Resources\Base\Instructor\TagResource;
 use App\Model\Chapter;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Resources\Json\JsonResource;

0ddaadcd8b1181f51dbce837308ddf92de48bd49
yuu-mt <yuu.is3mt@gmail.com>
2025-04-08 02:59:38 +0900

    　jka-1181 ロジックの実装

--- app/Model/Instructor.php
+++ app/Model/Instructor.php
@@ -91,13 +91,14 @@ class Instructor extends Authenticatable
      * 配下の講師を取得
      *
      * @return BelongsToMany<Instructor, $this>
+     * @return HasMany<Tag>
      */
     public function managings(): BelongsToMany
     {
         return $this->belongsToMany(Instructor::class, 'manage_instructors', 'manager_id', 'instructor_id');
     }
 
-    public function tags()
+    public function tags(): HasMany
     {
         return $this->hasMany(Tag::class);
     }

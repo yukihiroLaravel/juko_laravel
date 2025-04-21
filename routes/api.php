2c35a88751d21904bf163c3a21573778503c3122
Katsuhiro Sakurai <info@nestle-design-office.com>
2025-04-21 21:32:14 +0900

    講師側 講座分類 削除API

--- routes/api.php
+++ routes/api.php
@@ -74,6 +74,7 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
                 Route::post('/', [App\Http\Controllers\Api\Instructor\CourseController::class, 'store']);
                 Route::put('status', [App\Http\Controllers\Api\Instructor\CourseController::class, 'putStatus']);
                 Route::get('tag/index', [App\Http\Controllers\Api\Instructor\Course\TagController::class, 'index']);
+                Route::delete('tag/{tag_id}', [App\Http\Controllers\Api\Instructor\TagController::class, 'delete']);
                 Route::prefix('{course_id}')->group(function () {
                     Route::get('/', [App\Http\Controllers\Api\Instructor\CourseController::class, 'show']);
                     Route::post('/', [App\Http\Controllers\Api\Instructor\CourseController::class, 'update']);

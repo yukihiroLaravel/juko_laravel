7677aa7c5e950b7efb4b115ad06211ebb41a4d68
yurika nagaishi <lucky_c_b_1002@icloud.com>
2025-04-22 19:46:30 +0900

    jka-1158フォームリクエスト作成修正2(yurika)

--- app/Http/Controllers/Api/Manager/TagController.php
+++ app/Http/Controllers/Api/Manager/TagController.php
@@ -49,14 +49,14 @@ class TagController extends Controller
     }
 
     //タグ詳細API
-    public function show(ShowRequest $request, $id)
+    public function show(ShowRequest $request): JsonResponse
     {
         $instructorId = Auth::guard('instructor')->user()->id;
         $manager = Instructor::with('managings')->find($instructorId);
         $instructorIds = $manager->managings->pluck('id')->toArray();
         $instructorIds[] = $instructorId;
 
-        $tag = Tag::findOrFail($id);
+        $tag = Tag::findOrFail($request->tag_id);
 
         if (! in_array($tag->instructor_id, $instructorIds, true)) {
             // 自分、または配下の講師の講座でなければエラー応答

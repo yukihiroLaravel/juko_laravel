6d229116a25344ffb2cb719ddb8c1b60016beb29
Kou <hikkappi@yahoo.co.jp>
2025-05-16 20:18:15 +0900

    Remove: phpdocのパラメータ説明を削除

--- app/Services/Lesson/UpdateLessonService.php
+++ app/Services/Lesson/UpdateLessonService.php
@@ -8,13 +8,6 @@ class UpdateLessonService
 {
     /**
      * レッスン内容を更新する
-     *
-     * @param array{
-     *     title: string,
-     *     url: string,
-     *     remarks: string|null,
-     *     status: string
-     * } $data 更新するデータ
      */
     public function __invoke(Lesson $lesson, string $title, string $url, ?string $remarks, string $status): void
     {

77d086193ca63696c033560cd7c0e398f60a9adb
miyagi <yagi3885@gmail.com>
2025-05-16 11:31:46 +0900

    JKA-1224 チャプター並び順更新サービスクラスの実装

---
+++ app/Services/Chapter/SortChaptersService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services\Chapter;
+
+use App\Model\Chapter;
+
+class SortChaptersService
+{
+    /**
+     * チャプターの並び順を更新する
+     * 
+     * @param array $chapters チャプターIDと並び順を含む配列
+     * @param int $courseId 講座ID
+     */
+    public function __invoke(array $chapters, int $courseId): void
+    {
+        foreach ($chapters as $chapter) {
+            Chapter::where('id', $chapter['chapter_id'])
+                ->where('course_id', $courseId)
+                ->firstOrFail()
+                ->update([
+                    'order' => $chapter['order'],
+                ]);
+        }
+    }
+}

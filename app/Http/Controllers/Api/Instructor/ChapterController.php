996f59f5d675b7ecd39d4632ef0906dbfe34ca6b
KzAshimo <good.ashimo@gmail.com>
2025-05-21 22:44:20 +0900

    Revert "Merge branch 'develop' of https://github.com/yukihiroLaravel/juko_laravel into feature/jka-1296/ashino/instructor_tag_delete_controller"
    This reverts commit 38ef1b5d92c74c8e8d1310806a44050d985f50e7, reversing
    changes made to b2ab58c4ab30ed70172feb775d9c445dc373c162.

--- app/Http/Controllers/Api/Instructor/ChapterController.php
+++ app/Http/Controllers/Api/Instructor/ChapterController.php
@@ -21,7 +21,6 @@ use App\Services\Chapter\BulkDeleteChapterService;
 use App\Services\Chapter\CreateChapterService;
 use App\Services\Chapter\QueryService;
 use App\Services\Chapter\SortChaptersService;
-use App\Services\Chapter\UpdateAllChaptersStatusService;
 use App\Services\Chapter\UpdateChapterService;
 use Exception;
 use Illuminate\Auth\Access\AuthorizationException;
@@ -281,17 +280,17 @@ class ChapterController extends Controller
     /**
      * チャプター一括更新API
      */
-    public function putStatus(PutStatusRequest $request, UpdateAllChaptersStatusService $service): JsonResponse
+    public function putStatus(PutStatusRequest $request): JsonResponse
     {
         /** @var Course $course */
         $course = Course::findOrFail($request->course_id);
 
         if (Auth::guard('instructor')->user()->id !== $course->instructor_id) {
             // ログインしていない講師の更新を許可しない
-            throw new AuthorizationException('Forbidden, invalid course_id.');
+            throw new AuthorizationException('Not authorized.');
         }
 
-        $service($request->course_id, $request->status);
+        Chapter::chapterUpdateAll($request->course_id, $request->status);
 
         return response()->json([
             'result' => true,

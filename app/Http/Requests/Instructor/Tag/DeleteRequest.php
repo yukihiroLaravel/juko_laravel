364d26965a13919552134197897d42acfe7cbbb0
KzAshimo <good.ashimo@gmail.com>
2025-05-22 15:22:46 +0900

    controllre requst route 実装

---
+++ app/Http/Requests/Instructor/Tag/DeleteRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests\Instructor\Tag;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class DeleteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * バリデーション前にルートパラメータをマージ
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'tag_id' => $this->route('tag_id'),
+        ]);
+    }
+
+    /**
+     * バリデーションルール
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'tag_id' => ['required', 'integer', 'exists:tags,id'],
+        ];
+    }
+
+    /**
+     * バリデーションエラーメッセージ
+     */
+    public function messages(): array
+    {
+        return [
+            'tag_id.required' => 'タグIDは必須です。',
+            'tag_id.integer' => 'タグIDは整数である必要があります。',
+            'tag_id.exists' => '指定されたタグが存在しません。',
+        ];
+    }
+}

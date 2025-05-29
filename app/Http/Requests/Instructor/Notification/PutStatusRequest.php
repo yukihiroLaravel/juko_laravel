02d739c0d17cd3aa8eea4838f64848386aebe644
kosuke.chimata <chimatakousuke@gmail.com>
2025-05-29 22:30:51 +0900

    バリデーション内容作成

--- app/Http/Requests/Instructor/Notification/PutStatusRequest.php
+++ app/Http/Requests/Instructor/Notification/PutStatusRequest.php
@@ -3,8 +3,27 @@
 namespace App\Http\Requests\Instructor\Notification;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use App\Enums\NotificationStatus;
 
 class PutStatusRequest extends FormRequest
 {
-    //
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+        /**
+        * Get the validation rules that apply to the request.
+        *
+        * @return array<string, mixed>
+        */
+    public function rules(): array
+    {
+        return [
+            'status' => ['required', Rule::enum(NotificationStatus::class)],
+            'notifications' => ['required', 'array', 'min:1'],
+            'notifications.*' => ['integer', 'exists:notifications,id']
+        ];
+    }
 }

c2b5c999daeaa00cfc1af7009a9b08123947cda7
KzAshimo <good.ashimo@gmail.com>
2025-05-30 22:40:13 +0900

    request修正

--- app/Http/Requests/Instructor/Notification/StoreRequest.php
+++ app/Http/Requests/Instructor/Notification/StoreRequest.php
@@ -4,7 +4,6 @@ namespace App\Http\Requests\Instructor\Notification;
 
 use App\Enums\Notification\StatusEnum;
 use App\Enums\Notification\TypeEnum;
-use App\Rules\NotificationStoreStatusRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 

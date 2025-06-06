8f92a47121461d9d30f76ca2c2ff374e084d3946
KzAshimo <good.ashimo@gmail.com>
2025-06-06 10:46:35 +0900

    policy修正

--- app/Policies/CoursePolicy.php
+++ app/Policies/CoursePolicy.php
@@ -7,21 +7,15 @@ use App\Model\Instructor;
 
 class CoursePolicy
 {
-    /**
-     * Determine whether the instructor can delete the course.
-     */
-    public function delete(Instructor $instructor, Course $course): bool
+    public function update(Instructor $instructor, Course $course): bool
     {
-        // マネージャー権限のある講師か判定
         if ($instructor->isManager()) {
-            $manager = Instructor::with('managings')->find($instructor->id);
-            $instructorIds = $manager->managings->pluck('id')->toArray();
-            $instructorIds[] = $instructor->id;
-
-            return in_array($course->instructor_id, $instructorIds, true);
+            // 管理者の場合、配下の講師の講座も更新可能
+            $managerIds = $instructor->managings->pluck('id')->toArray();
+            $managerIds[] = $instructor->id;
+            return in_array($course->instructor_id, $managerIds, true);
         }
-
-        // マネージャー権限のない講師
+        // 講師の場合、自分の講座のみ更新可能
         return $instructor->id === $course->instructor_id;
     }
 }

09c4d5448e179e71a1d5fdaf89bfa774b8a0307c
yurika nagaishi <lucky_c_b_1002@icloud.com>
2025-06-06 11:20:51 +0900

    講師側お知らせ更新APIを修正して公開・非公開を更新3

--- app/Http/Requests/Instructor/Notification/PutRequest.php
+++ app/Http/Requests/Instructor/Notification/PutRequest.php
@@ -39,6 +39,7 @@ class PutRequest extends FormRequest
             'end_date' => ['required', 'date_format:Y-m-d H:i:s', 'after:start_date'],
             'title' => ['required', 'string', 'max:50'],
             'content' => ['required', 'string', 'max:500'],
+            'status' => 'required|in:public,private',// ← （公開・非公開の値）
         ];
     }
 }

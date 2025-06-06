b31f92c440b4f2401ce760af0174534ae273a0a9
kosuke.chimata <chimatakousuke@gmail.com>
2025-06-07 01:42:47 +0900

    developブランチ脱出

--- app/Http/Controllers/Api/Manager/CourseController.php
+++ app/Http/Controllers/Api/Manager/CourseController.php
@@ -97,23 +97,16 @@ class CourseController extends Controller
     /**
      * 講座登録API
      */
-    public function store(StoreRequest $request): JsonResponse
+    public function store(StoreRequest $request, CreateCourseService $createCourseService): JsonResponse
     {
         $managerId = Auth::guard('instructor')->user()->id;
 
-        $file = $request->file('image');
-        $extension = $file->getClientOriginalExtension();
-        $filename = Str::uuid()->toString().'.'.$extension;
-        $filePath = Storage::disk('public')->putFileAs('course', $file, $filename);
-
-        $course = Course::create([
-            'instructor_id' => $managerId,
-            'title' => $request->title,
-            'image' => $filePath,
-            'status' => Course::STATUS_PRIVATE,
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
-        ]);
+        $course = $createCourseService(
+            title: $request->title,
+            image: $request->file('image'),
+            tagId: $request->tag_id,
+            instructorOrManagerId: $managerId
+        );
 
         return response()->json([
             'result' => true,

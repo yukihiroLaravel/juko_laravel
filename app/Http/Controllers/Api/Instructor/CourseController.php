dec17f051331dc0840887d3710b5f217a3cf0c67
KzAshimo <good.ashimo@gmail.com>
2025-06-06 14:21:13 +0900

    student側controller エラーハンドリング修正

--- app/Http/Controllers/Api/Instructor/CourseController.php
+++ app/Http/Controllers/Api/Instructor/CourseController.php
@@ -193,7 +193,9 @@ class CourseController extends Controller
             return response()->json([
                 'result' => true,
             ]);
-        } catch (Exception $e) {
+        }catch(AuthorizationException $e){
+            throw $e;
+        }catch (Exception $e) {
             Log::error($e);
             throw $e;
         }

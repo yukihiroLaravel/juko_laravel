7669c7343d255482b715bf9f1e5649a59215b18c
kosuke.chimata <chimatakousuke@gmail.com>
2025-06-10 02:30:09 +0900

    filePathのスルーをExceptionに変更2

--- app/Services/Course/StoreCourseService.php
+++ app/Services/Course/StoreCourseService.php
@@ -22,7 +22,7 @@ class StoreCourseService
         $filename = Str::uuid()->toString().'.'.$extension;
         $filePath = Storage::putFileAs('public/course', $file, $filename);
         if (! $filePath) {
-            throw new Exception('ファイル保存に失敗しました。');
+            throw new Exception('Unable to write file');
         }
         $filePath = Course::convertImagePath($filePath);
 

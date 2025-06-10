8a69f7ada27f0f761537e49bab79dee5fc0c2b5f
KzAshimo <good.ashimo@gmail.com>
2025-06-10 13:03:26 +0900

    policy修正

--- app/Policies/ChapterPolicy.php
+++ app/Policies/ChapterPolicy.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Policies\Policies;
+namespace App\Policies;
 
 use App\Model\Chapter;
 use App\Model\Instructor;
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use App\Model\Chapter;
 use App\Model\Course;
 use App\Policies\CoursePolicy;
-use App\Policies\Policies\ChapterPolicy;
+use App\Policies\ChapterPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider

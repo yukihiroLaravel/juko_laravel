ccd9f4ff569cacf7d641c0535af0dc2fa2fa712f
KzAshimo <good.ashimo@gmail.com>
2025-06-10 11:09:02 +0900

    1.マッピング 2.policy作成

---
+++ app/Policies/Policies/ChapterPolicy.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Policies\Policies;
+
+class ChapterPolicy
+{
+    /**
+     * チャプター削除
+     */
+    public function delete()
+    {
+        //
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Model\Chapter;
 use App\Model\Course;
 use App\Policies\CoursePolicy;
+use App\Policies\Policies\ChapterPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -15,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Course::class => CoursePolicy::class,
+        Chapter::class => ChapterPolicy::class,
     ];
 
     /**

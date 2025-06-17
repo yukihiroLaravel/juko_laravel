6601ca9cc31dc89c82d8acbf99c4cbb255951fc9
KzAshimo <good.ashimo@gmail.com>
2025-06-18 04:49:41 +0900

    1.routing 2.controller 空配列返却

--- app/Http/Controllers/Api/Instructor/NotificationController.php
+++ app/Http/Controllers/Api/Instructor/NotificationController.php
@@ -197,6 +197,14 @@ class NotificationController extends Controller
         }
     }
 
+        /**
+     * 該当講師お知らせ一覧タイプ　一括変更
+     */
+    public function updateTypeAll()
+    {
+        return response()->json([]);
+    }
+
     /**
      * お知らせ一括削除
      */

875c71b1e7d0a23c69aaafef7803c33869dd730f
KzAshimo <good.ashimo@gmail.com>
2025-06-20 22:13:06 +0900

    NotificationPolicy実装 マッピング

---
+++ app/Policies/NotificationPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Model\Instructor;
+use App\Model\Notification;
+
+class NotificationPolicy
+{
+    /**
+     * お知らせの更新処理に関する認可処理
+     */
+    public function update(Instructor $instructor, Notification $notification)
+    {
+        // マネージャーの場合は配下の講師レッスンも更新可能
+        if($instructor->isManager()){
+            $manager = Instructor::with('managings')->find($instructor->id);
+            $instructorIds = $manager->managings->pluck('id')->toArray();
+            $instructorIds[] = $instructor->id;
+
+            return is_array($notification->instructor_id, $instructorIds, true);
+        }
+
+        // マネージャー権限のない講師の場合は自分のレッスンのみ更新可能
+        return $notification->instructor_id === $instructor->id;
+    }
+}

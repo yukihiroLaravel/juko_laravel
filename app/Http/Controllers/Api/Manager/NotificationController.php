33d82d5a27cf3f54d4bfcd4fcecdb865a651359d
Kou <hikkappi@yahoo.co.jp>
2025-06-21 07:56:58 +0900

    Change: NotificationControllerの更新メソッドをputに変更し、PutRequestを新規作成

--- app/Http/Controllers/Api/Manager/NotificationController.php
+++ app/Http/Controllers/Api/Manager/NotificationController.php
@@ -6,9 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Manager\Notification\BulkDeleteRequest;
 use App\Http\Requests\Manager\Notification\DeleteRequest;
 use App\Http\Requests\Manager\Notification\IndexRequest;
+use App\Http\Requests\Manager\Notification\PutRequest;
 use App\Http\Requests\Manager\Notification\ShowRequest;
 use App\Http\Requests\Manager\Notification\StoreRequest;
-use App\Http\Requests\Manager\Notification\UpdateRequest;
 use App\Http\Requests\Manager\Notification\UpdateTypeRequest;
 use App\Http\Resources\Base\Instructor\NotificationResource;
 use App\Http\Resources\Manager\NotificationIndexResource;
@@ -124,11 +124,10 @@ class NotificationController extends Controller
     /**
      * お知らせ更新API
      */
-    public function update(UpdateRequest $request): JsonResponse
+    public function put(PutRequest $request): JsonResponse
     {
         // 指定されたお知らせIDでお知らせを取得
         $notification = Notification::with('course')->findOrFail($request->notification_id);
-        assert($notification instanceof Notification);
 
         // policyによる認可チェック
         $this->authorize('update', $notification);

800fb5fc33d52e2a62dad0c5674cdab232052191
KzAshimo <good.ashimo@gmail.com>
2025-06-22 21:28:13 +0900

    policyコメント修正

--- app/Policies/TagPolicy.php
+++ app/Policies/TagPolicy.php
@@ -12,7 +12,7 @@ class TagPolicy
      */
     public function delete(Instructor $instructor, Tag $tag)
     {
-        // マネージャーの場合は配下の講師レッスンも更新可能
+        // マネージャーの場合は配下の講師タグも削除可能
         if ($instructor->isManager()) {
             $instructorIds = $instructor->managings->pluck('id')->toArray();
             $instructorIds[] = $instructor->id;
@@ -20,7 +20,7 @@ class TagPolicy
             return in_array($tag->instructor_id, $instructorIds, true);
         }
 
-        // マネージャー権限のない講師の場合は自分のレッスンのみ更新可能
+        // マネージャー権限のない講師の場合は自分のタグのみ削除可能
         return $tag->instructor_id === $instructor->id;
 
     }

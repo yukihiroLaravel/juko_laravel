f1866d1baab833d0d2258e75f9f018c41b13d83c
KzAshimo <good.ashimo@gmail.com>
2025-06-22 14:34:07 +0900

    1.マッピング 2.controller修正 3.policy作成

---
+++ app/Policies/TagPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Model\Instructor;
+use App\Model\Tag;
+
+class TagPolicy
+{
+    /**
+     * タグの削除機能に関する認可処理
+     */
+    public function delete(Instructor $instructor, Tag $tag)
+    {
+        // マネージャーの場合は配下の講師レッスンも更新可能
+        if ($instructor->isManager()) {
+            $instructorIds = $instructor->managings->pluck('id')->toArray();
+            $instructorIds[] = $instructor->id;
+
+            return in_array($tag->instructor_id, $instructorIds, true);
+        }
+
+        // マネージャー権限のない講師の場合は自分のレッスンのみ更新可能
+        return $tag->instructor_id === $instructor->id;
+
+    }
+}

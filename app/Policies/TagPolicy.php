4dc74bf28e89a22b9c02edc2812df78258d6fda7
kosuke.chimata <chimatakousuke@gmail.com>
2025-06-25 01:45:16 +0900

    間違って上書きしたdelete()を復活

--- app/Policies/TagPolicy.php
+++ app/Policies/TagPolicy.php
@@ -8,8 +8,24 @@ use App\Model\Tag;
 class TagPolicy
 {
     /**
+     * タグの削除機能に関する認可処理
+     */
+    public function delete(Instructor $instructor, Tag $tag)
+    {
+        // マネージャーの場合は配下の講師タグも削除可能
+        if ($instructor->isManager()) {
+            $instructorIds = $instructor->managings->pluck('id')->toArray();
+            $instructorIds[] = $instructor->id;
 
-     * Create a new policy instance.
+            return in_array($tag->instructor_id, $instructorIds, true);
+        }
+
+        // マネージャー権限のない講師の場合は自分のタグのみ削除可能
+        return $tag->instructor_id === $instructor->id;
+    }
+
+    /**
+     * タグの更新機能に関する認可処理
      */
     public function update(Instructor $instructor, Tag $tag): bool
     {

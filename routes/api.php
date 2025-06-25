070bdd819aa92ffd61280bc859a9051b3de58c46
jin 6hara <job6hara@gmail.com>
2025-06-25 23:52:17 +0900

    api.phpにしたメモを削除（復元）

--- routes/api.php
+++ routes/api.php
@@ -79,9 +79,9 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
                     // 講師-講座-チャプター
                     Route::prefix('chapter')->group(function () {
                         Route::post('/', [App\Http\Controllers\Api\Instructor\ChapterController::class, 'store']);
-                        Route::post('sort', [App\Http\Controllers\Api\Instructor\ChapterController::class, 'sort']);//jka-1370
-                        Route::put('status', [App\Http\Controllers\Api\Instructor\ChapterController::class, 'putStatus']);//jka-1370
-                        Route::patch('status', [App\Http\Controllers\Api\Instructor\ChapterController::class, 'patchStatus']);//jka-1370
+                        Route::post('sort', [App\Http\Controllers\Api\Instructor\ChapterController::class, 'sort']);
+                        Route::put('status', [App\Http\Controllers\Api\Instructor\ChapterController::class, 'putStatus']);
+                        Route::patch('status', [App\Http\Controllers\Api\Instructor\ChapterController::class, 'patchStatus']);
                         Route::delete('/', [App\Http\Controllers\Api\Instructor\ChapterController::class, 'bulkDelete']);
                         Route::delete('all', [App\Http\Controllers\Api\Instructor\ChapterController::class, 'deleteAll']);
                         Route::prefix('{chapter_id}')->group(function () {
@@ -203,17 +203,17 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
                         Route::delete('/', [App\Http\Controllers\Api\Manager\CourseController::class, 'delete']);
                         // マネージャー-講座-チャプター
                         Route::prefix('chapter')->group(function () {
-                            Route::post('sort', [App\Http\Controllers\Api\Manager\ChapterController::class, 'sort']);//jka-1370
+                            Route::post('sort', [App\Http\Controllers\Api\Manager\ChapterController::class, 'sort']);
                             Route::post('/', [App\Http\Controllers\Api\Manager\ChapterController::class, 'store']);
-                            Route::put('status', [App\Http\Controllers\Api\Manager\ChapterController::class, 'putStatus']);//jka-1370
+                            Route::put('status', [App\Http\Controllers\Api\Manager\ChapterController::class, 'putStatus']);
                             Route::delete('/', [App\Http\Controllers\Api\Manager\ChapterController::class, 'bulkDelete']);
                             Route::delete('all', [App\Http\Controllers\Api\Manager\ChapterController::class, 'deleteAll']);
-                            Route::patch('status', [App\Http\Controllers\Api\Manager\ChapterController::class, 'patchStatus']);//jka-1370
+                            Route::patch('status', [App\Http\Controllers\Api\Manager\ChapterController::class, 'patchStatus']);
                             Route::prefix('{chapter_id}')->group(function () {
                                 Route::get('/', [App\Http\Controllers\Api\Manager\ChapterController::class, 'show']);
                                 Route::put('/', [App\Http\Controllers\Api\Manager\ChapterController::class, 'put']);
                                 Route::delete('/', [App\Http\Controllers\Api\Manager\ChapterController::class, 'delete']);
-                                Route::patch('status', [App\Http\Controllers\Api\Manager\ChapterController::class, 'updateStatus']);//jka-1370
+                                Route::patch('status', [App\Http\Controllers\Api\Manager\ChapterController::class, 'updateStatus']);/
 
                                 // マネージャー-講座-チャプター-レッスン
                                 Route::prefix('lesson')->group(function () {

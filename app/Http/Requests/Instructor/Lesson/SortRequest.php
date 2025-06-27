73bc62898759ce422dc8c9e6167fdbf25172e751
kosuke.chimata <chimatakousuke@gmail.com>
2025-06-28 03:16:00 +0900

    6/28 kouさんにコメント意味の確認中　訂正前

--- app/Http/Requests/Instructor/Lesson/SortRequest.php
+++ app/Http/Requests/Instructor/Lesson/SortRequest.php
@@ -29,7 +29,8 @@ class SortRequest extends FormRequest
             'course_id' => ['required', 'integer', 'exists:courses,id,deleted_at,NULL'],
             'chapter_id' => ['required', 'integer', 'exists:chapters,id,deleted_at,NULL'],
             'lessons' => ['required', 'array'],
-            'lessons.*.lesson_id' => ['required', 'integer', 'exists:lessons,id,deleted_at,NULL'],
+            // order値を検証する。
+            'lessons.*.order' => ['required', 'integer', 'exists:lessons,id,deleted_at,NULL'],
         ];
     }
 

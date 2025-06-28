32c983b01c54e7cf526b16b606cc11de1bd4bed8
kosuke.chimata <chimatakousuke@gmail.com>
2025-06-29 03:31:05 +0900

    6/29 SortLessonsServicesでcomposer analyzeの型エラーが出る（）。これを回避するために、アノテーションを記述。

--- app/Services/Lesson/SortLessonsService.php
+++ app/Services/Lesson/SortLessonsService.php
@@ -10,16 +10,11 @@ class SortLessonsService
 {
     /**
      * レッスン並び替えサービス order値は、配列要素のインデックスにする。１から始まる値にする。
+     * @param \Illuminate\Database\Eloquent\Collection<int, \App\Model\Lesson> $lessons
      */
     public function __invoke(Collection $lessons, array $inputLessons): void
     {
-        $lessons->each(function (Model $model) use ($inputLessons) {
-            if (! $model instanceof Lesson) {
-                return;
-            }
-
-            $lesson = $model;
-
+        $lessons->each(function (Lesson $lesson) use ($inputLessons) {
             // lessons配列内における要素のインデックスを取得
             $index = array_search($lesson->id, $inputLessons, true);
 

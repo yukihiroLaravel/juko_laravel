14f2e1a8fbb71c394246795a871e683662983f80
kosuke.chimata <chimatakousuke@gmail.com>
2025-06-30 01:13:40 +0900

    6/29 bulkDelete()のテストファイル作成

---
+++ tests/Feature/Api/Manager/Notification/BulkDeleteTest.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace Tests\Feature\Api\Instructor\Notification;
+
+use App\Model\Instructor;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DeleteTest extends TestCase
+{
+    use RefreshDatabase;
+
+    // setup
+    #[\Override]
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->seed();
+    }
+
+    public function test_お知らせ削除_成功(): void
+    {
+        // arrange
+        $instructor = Instructor::find(2);
+        $this->actingAs($instructor, 'instructor');
+
+        // act
+        $response = $this->deleteJson('/api/v1/instructor/notification/2');
+
+        // assert
+        $response->assertStatus(200);
+        $response->assertJson([
+            'result' => true,
+        ]);
+
+        // リレーションされているデータが削除されているか確認
+        $this->assertDatabaseMissing('viewed_once_notifications', [
+            'id' => 1,
+        ]);
+    }
+
+    public function test_お知らせ一括削除_成功(): void
+    {
+        // arrange
+        $instructor = Instructor::find(1);
+        $this->actingAs($instructor, 'instructor');
+
+        // act
+        $response = $this->deleteJson('/api/v1/instructor/notification', [
+            'notifications' => [1, 2],
+        ]);
+
+        // assert
+        $response->assertStatus(200);
+        $response->assertJson([
+            'result' => true,
+        ]);
+        $this->assertSoftDeleted('notifications', [
+            'id' => 1,
+        ]);
+        $this->assertSoftDeleted('notifications', [
+            'id' => 2,
+        ]);
+        $this->assertDatabaseMissing('viewed_once_notifications', [
+            'id' => 1,
+        ]);
+    }
+
+    public function test_権限がない講師_失敗(): void
+    {
+        // arrange
+        $instructor = Instructor::find(2);
+        $this->actingAs($instructor, 'instructor');
+
+        // act
+        $response = $this->deleteJson('/api/v1/instructor/notification/1');
+
+        // assert
+        $response->assertStatus(403);
+        $response->assertJson([
+            'message' => 'Invalid instructor_id.',
+        ]);
+    }
+
+    public function test_バリデーションエラー１(): void
+    {
+        // arrange
+        $instructor = Instructor::find(2);
+        $this->actingAs($instructor, 'instructor');
+
+        // act
+        $response = $this->deleteJson('/api/v1/instructor/notification/aaa');
+
+        // assert
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors([
+            'notification_id',
+        ]);
+    }
+
+    public function test_バリデーションエラー２(): void
+    {
+        // arrange
+        $instructor = Instructor::find(2);
+        $this->actingAs($instructor, 'instructor');
+
+        // act
+        $response = $this->deleteJson('/api/v1/instructor/notification', [
+            'notifications' => [],
+        ]);
+
+        // assert
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors([
+            'notification_id',
+        ]);
+    }
+}

1522f6561e4082d07bd0f7ea79ddcf8c756fe26b
jin 6hara <job6hara@gmail.com>
2025-07-16 10:43:40 +0900

    NotificationControllerのupdateTypeAllメソッドをサービスクラス作成で共通化

---
+++ app/Services/Notification/UpdateTypeAllService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services\Notification;
+
+use App\Model\Notification;
+use Illuminate\Support\Facades\Log;
+
+class UpdateTypeAllService
+{
+    /**
+     * 指定されたinstructorId配列に属する通知タイプを一括更新
+     */
+    public function updateNotificationType(array $instructorIds, string $notificationType): bool
+    {
+        try {
+            Notification::whereIn('instructor_id', $instructorIds)->update([
+                'type' => $notificationType,
+            ]);
+            return true;
+        } catch (\Exception $e) {
+            Log::error('Notification update failed: ' . $e->getMessage());
+            throw $e;
+        }
+    }
+}

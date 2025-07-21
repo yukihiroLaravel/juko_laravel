bec135c1631bce7ba49c228d7a012ff39e82066d
Morio151515 <m.morio@rhythm.ocn.ne.jp>
2025-07-21 10:55:13 +0000

    fix: add @throws AuthorizationException

--- app/Services/Notification/UpdateTypeService.php
+++ app/Services/Notification/UpdateTypeService.php
@@ -3,7 +3,9 @@
 namespace App\Services\Notification;
 
 use App\Model\Notification;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 
 class UpdateTypeService
 {
@@ -11,9 +13,11 @@ class UpdateTypeService
      * 通知タイプを一括更新
      *
      * @param  Collection<int, Notification>  $notifications
+     *
+     * @throws AuthorizationException
      */
     public function __invoke(Collection $notifications, string $type): void
-    {
+    {  
         Notification::whereIn('id', $notifications->pluck('id'))
             ->update(['type' => $type]);
     }

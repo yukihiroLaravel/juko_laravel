1c55981eff5d9ad70fd10faba2c60499e280cc41
tomomi yashiki <wagona3667@gmail.com>
2025-07-23 00:51:38 +0900

    JKA-1369 講師/マネージャー チャプター削除機能へのPolicyパターンを用いた認可機能の実装 修正3

--- app/Http/Requests/Instructor/Chapter/DeleteRequest.php
+++ app/Http/Requests/Instructor/Chapter/DeleteRequest.php
@@ -6,15 +6,35 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class DeleteRequest extends FormRequest
 {
-    public function authorize(): bool
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
     {
-        return true; // 認可は Controller や Policy 側で行う
+        return true;
     }
 
-    public function rules(): array
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
     {
         return [
-            'course_id' => ['required', 'integer', 'exists:courses,id,deleted_at,NULL']
+            'course_id' => ['required', 'integer', 'exists:courses,id,deleted_at,NULL'],
+            'chapter_id' => ['required', 'integer', 'exists:chapters,id,deleted_at,NULL'],
         ];
+    }
+
+    #[\Override]
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'course_id' => $this->route('course_id'),
+            'chapter_id' => $this->route('chapter_id'),
+        ]);
     }
 }

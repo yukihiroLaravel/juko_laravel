2c221381cc034637baec6c1cee34018bd6732613
mitsunobu morio <m.morio@rhythm.ocn.ne.jp>
2025-07-30 22:36:54 +0900

    fix: NotificationPolicy correction16

--- app/Http/Controllers/Api/Instructor/NotificationController.php
+++ app/Http/Controllers/Api/Instructor/NotificationController.php
@@ -32,6 +32,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Gate;
 
 /**
  * @tags Instructor-Notification
--- app/Http/Controllers/Api/Manager/NotificationController.php
+++ app/Http/Controllers/Api/Manager/NotificationController.php
@@ -33,6 +33,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Gate;
 
 /**
  * @tags Manager-Notification

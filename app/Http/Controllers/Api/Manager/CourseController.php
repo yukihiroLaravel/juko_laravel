4be08618fcc0e48f1a557c0baf32f8530f7dd388
Kou <hikkappi@yahoo.co.jp>
2025-07-31 23:17:04 +0900

    Refactor: CourseControllerのshowメソッドからQueryServiceを削除し、直接Courseモデルを使用するように変更 Refactor: CoursePolicyのviewメソッドとupdateメソッドのロジックを整理 Remove: QueryServiceを削除 Test: ShowTestの講師IDを修正し、権限チェックメッセージを更新

--- app/Http/Controllers/Api/Manager/CourseController.php
+++ app/Http/Controllers/Api/Manager/CourseController.php
@@ -15,7 +15,6 @@ use App\Model\Course;
 use App\Model\Instructor;
 use App\Services\Course\DeleteService;
 use App\Services\Course\PutStatusService;
-use App\Services\Course\QueryService;
 use App\Services\Course\StoreCourseService;
 use App\Services\Course\UpdateCourseService;
 use Exception;
@@ -74,9 +73,9 @@ class CourseController extends Controller
     /**
      * 講座情報取得API
      */
-    public function show(ShowRequest $request, QueryService $queryService): CourseShowResource
+    public function show(ShowRequest $request): CourseShowResource
     {
-        $course = $queryService->getCourse($request->course_id);
+        $course = Course::with(['chapters.lessons'])->findOrFail($request->course_id);
 
         // 認可チェック
         $this->authorize('view', $course);

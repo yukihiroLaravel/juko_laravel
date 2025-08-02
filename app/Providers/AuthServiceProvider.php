4ee73d616f03f4c20afeb8c5353c2f75c1e658ad
tomomi yashiki <wagona3667@gmail.com>
2025-08-02 11:34:22 +0900

    JKA-1514 StudentControllerのshowメソッドのPolicyパターンを用いた認可機能の実装 修正2

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,12 +8,14 @@ use App\Model\Course;
 use App\Model\Lesson;
 use App\Model\Notification;
 use App\Model\Tag;
+use App\Model\Student;
 use App\Policies\AttendancePolicy;
 use App\Policies\ChapterPolicy;
 use App\Policies\CoursePolicy;
 use App\Policies\LessonPolicy;
 use App\Policies\NotificationPolicy;
 use App\Policies\TagPolicy;
+use App\Policies\StudentPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider

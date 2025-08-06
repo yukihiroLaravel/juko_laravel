8b71829247fdcf66d5ce13af98104d6aaa72353a
tomomi yashiki <wagona3667@gmail.com>
2025-08-06 13:24:28 +0900

    JKA-1521 coursesテーブルの受講期限カラム反映→講師側 講座登録APIの修正 修正2

--- database/migrations/2022_10_19_215031_create_courses_table.php
+++ database/migrations/2022_10_19_215031_create_courses_table.php
@@ -19,11 +19,11 @@ class CreateCoursesTable extends Migration
             $table->string('title', 50)->comment('タイトル');
             $table->text('image')->comment('サムネイルファイルパス');
             $table->string('status', 30)->comment('ステータス');
+            $table->dateTime('attendance_deadline')->nullable()->comment('受講期限');
             $table->dateTime('created_at');
             $table->dateTime('updated_at');
             $table->softDeletes();
             $table->foreign('instructor_id')->references('id')->on('instructors');
-            $table->dateTime('attendance_deadline')->nullable()->comment('受講期限');
         });
     }
 

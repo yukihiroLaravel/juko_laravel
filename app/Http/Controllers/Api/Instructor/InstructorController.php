8121e1e9c81abb1ba049f56796abc7d2fb0cc5ad
mitsunobu morio <m.morio@rhythm.ocn.ne.jp>
2025-08-11 22:57:10 +0900

    JKA-1531-2

--- app/Http/Controllers/Api/Instructor/InstructorController.php
+++ app/Http/Controllers/Api/Instructor/InstructorController.php
@@ -122,7 +122,6 @@ class InstructorController extends Controller
     {
         try {
             $instructor = Auth::user();
-            $this->authorize('update', $instructor);
 
             // 更新前の画像パスを使用
             $imagePath = $instructor->profile_image;
--- app/Policies/InstructorPolicy.php
+++ app/Policies/InstructorPolicy.php
@@ -9,69 +9,20 @@ use Illuminate\Auth\Access\Response;
 class InstructorPolicy
 {
     /**
-     * Determine whether the user can view any models.
-     */
-    public function viewAny(Student $student): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can view the model.
-     */
-    public function view(Student $student, Instructor $instructor): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(Student $student): bool
-    {
-        //
-    }
-
-    /**
      * マネージャーは配下の講師と自分自身を更新可能。
      * 講師は自分自身のみ更新可能。
      *
-     * @param  \App\Model\Instructor  $loginUser  ログイン中の講師
-     * @param  \App\Model\Instructor  $instructor  更新対象の講師
+     * @param  \App\Model\Instructor  $instructor  ログイン中、更新対象の講師     
      * @return bool
      */
-    public function update(Instructor $loginUser, Instructor $instructor)
+    public function update(Instructor $instructor, Instructor $instructor)
     {
-        if ($loginUser->isManager()) {
-            $instructorIds = $loginUser->managings->pluck('id')->toArray();
-            $instructorIds[] = $loginUser->id;
+        if ($instructor->isManager()) {
+            $instructorIds = $instructor->managings->pluck('id')->toArray();
+            $instructorIds[] = $instructor->id;
             return in_array($instructor->id, $instructorIds, true);
         }
 
-        return $loginUser->id === $instructor->id;
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(Student $student, Instructor $instructor): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(Student $student, Instructor $instructor): bool
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(Student $student, Instructor $instructor): bool
-    {
-        //
+        return $instructor->id === $instructor->id;
     }
 }

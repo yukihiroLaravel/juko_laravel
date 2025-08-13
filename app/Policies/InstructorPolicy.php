533dbc686bcf7372e346ed3bbf57937f048e6110
mitsunobu morio <m.morio@rhythm.ocn.ne.jp>
2025-08-13 13:34:30 +0900

    JKA-1531-5

--- app/Policies/InstructorPolicy.php
+++ app/Policies/InstructorPolicy.php
@@ -12,17 +12,18 @@ class InstructorPolicy
      * マネージャーは配下の講師と自分自身を更新可能。
      * 講師は自分自身のみ更新可能。
      *
-     * @param  \App\Model\Instructor  $instructor  ログイン中、更新対象の講師     
+     * @param  \App\Model\Instructor  $loginUser  ログイン中の講師 
+     * @param  \App\Model\Instructor  $instructor  更新対象の講師     
      * @return bool
      */
-    public function update(Instructor $instructor)
+    public function update(Instructor $loginUser, Instructor $instructor): bool
     {
-        if ($instructor->isManager()) {
-            $instructorIds = $instructor->managings->pluck('id')->toArray();
-            $instructorIds[] = $instructor->id;
+        if ($loginUser->isManager()) {
+            $instructorIds = $loginUser->managings->pluck('id')->toArray();
+            $instructorIds[] = $loginUser->id;
             return in_array($instructor->id, $instructorIds, true);
         }
 
-        return $instructor->id === $instructor->id;
+        return $loginUser->id === $instructor->id;
     }
 }

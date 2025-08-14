02f960c78efc5c1a0a67b0d09db0df8c8623a47a
tatsunori-1550 <aba31209875cc@gmail.com>
2025-08-15 06:20:47 +0900

    受講生側 お知らせ既読登録APIを修正して、受講期限切れの講座に紐づくお知らせを既読にしようとした際にエラーを出力するように対応

--- app/Services/Notification/MarkReadService.php
+++ app/Services/Notification/MarkReadService.php
@@ -5,6 +5,8 @@ namespace App\Services\Notification;
 use App\Enums\Notification\TypeEnum;
 use App\Model\Notification;
 use App\Model\Student;
+use Illuminate\Auth\Access\AuthorizationException;
+use Carbon\CarbonImmutable;
 
 class MarkReadService
 {
@@ -15,8 +17,17 @@ class MarkReadService
     {
         $notification = Notification::where('id', $notificationId)
             ->where('type', TypeEnum::ONCE)
-            ->with(['students'])
+            ->with(['students', 'course'])
             ->firstOrFail();
+
+        if (
+            $notification->course->attendance_deadline &&
+            CarbonImmutable::now()->gte(
+                $notification->course->attendance_deadline->endOfDay()
+            )
+        ) {
+            throw new AuthorizationException('The course has expired.');
+        }       
 
         // ユーザが確認したお知らせを登録(既読登録)
         if (! $notification->students->contains($student->id)) {

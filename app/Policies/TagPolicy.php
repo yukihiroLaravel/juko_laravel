8deca3067a15838292b4b0333ba144ecf578285d
Kou <hikkappi@yahoo.co.jp>
2025-08-17 21:14:24 +0900

    Refactor: TagPolicyのメソッド名を修正し、ShowTestのインストラクターIDを更新

--- app/Policies/TagPolicy.php
+++ app/Policies/TagPolicy.php
@@ -8,6 +8,23 @@ use App\Model\Tag;
 class TagPolicy
 {
     /**
+     * タグの詳細機能に関する認可処理
+     */
+    public function view(Instructor $instructor, Tag $tag): bool
+    {
+        // マネージャー権限のある講師か判定
+        if ($instructor->isManager()) {
+            $instructorIds = $instructor->managings->pluck('id')->toArray();
+            $instructorIds[] = $instructor->id;
+
+            return in_array($tag->instructor_id, $instructorIds, true);
+        }
+
+        // マネージャー権限のない講師
+        return $tag->instructor_id === $instructor->id;
+    }
+
+    /**
      * タグの削除機能に関する認可処理
      */
     public function delete(Instructor $instructor, Tag $tag)
@@ -28,23 +45,6 @@ class TagPolicy
      * タグの更新機能に関する認可処理
      */
     public function update(Instructor $instructor, Tag $tag): bool
-    {
-        // マネージャー権限のある講師か判定
-        if ($instructor->isManager()) {
-            $instructorIds = $instructor->managings->pluck('id')->toArray();
-            $instructorIds[] = $instructor->id;
-
-            return in_array($tag->instructor_id, $instructorIds, true);
-        }
-
-        // マネージャー権限のない講師
-        return $tag->instructor_id === $instructor->id;
-    }
-
-    /**
-     * タグの詳細機能に関する認可処理
-     */
-    public function view(Instructor $instructor, Tag $tag): bool
     {
         // マネージャー権限のある講師か判定
         if ($instructor->isManager()) {

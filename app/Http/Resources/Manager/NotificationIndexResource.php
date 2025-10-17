1b8668a9843c34b6f1e4ce89cf3d20a7412aae78
yoshitsugu <ynwaerjkdbl@gmail.com>
2025-10-18 00:18:15 +0900

    講師側 お知らせ一覧取得APIの修正 講座の受講期限の新設定「受講開始から⚪︎ヶ月有効」などを考慮して対応(なかよし)

--- app/Http/Resources/Manager/NotificationIndexResource.php
+++ app/Http/Resources/Manager/NotificationIndexResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Manager;
 
+use App\Http\Resources\Base\Instructor\CourseResource;
 use App\Http\Resources\Base\Instructor\InstructorResource;
 use App\Model\Notification;
 use Illuminate\Database\Eloquent\Collection;
@@ -50,7 +51,7 @@ class NotificationIndexResource extends JsonResource
             'start_date' => $notification->start_date,
             'end_date' => $notification->end_date,
             'status' => $notification->status,
-            'expiration_date' => $notification->course?->getDeadlineForInstructor(),
+            'course' => new CourseResource($notification->course),
         ])
             ->toArray();
     }

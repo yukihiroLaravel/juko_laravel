4177b4c0accc726d1afebb2c23f6b415529433f0
hideki kishimoto <hidekishi_15109858986721@yahoo.co.jp>
2025-10-27 00:29:36 +0900

    JKA-1561 講師側 受講状況取得APIの修正

--- app/Http/Controllers/Api/Manager/CourseController.php
+++ app/Http/Controllers/Api/Manager/CourseController.php
@@ -120,7 +120,7 @@ class CourseController extends Controller
     /**
      * 講座情報更新API
      */
-    public function update(UpdateRequest $request, UpdateService $service, \App\Services\Course\CalculateDeadlineService $calculateDeadline): JsonResponse
+    public function update(UpdateRequest $request, UpdateService $service, CalculateDeadlineService $calculateDeadline): JsonResponse
     {
         DB::beginTransaction();
 

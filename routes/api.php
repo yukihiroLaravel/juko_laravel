597ecaec7d3aa268e53916adfcbf8b70412c3a75
Kou <hikkappi@yahoo.co.jp>
2026-02-12 20:54:10 +0900

    Manager側の通知APIにおけるステータス更新メソッドの追加と不要なメソッドの削除

--- routes/api.php
+++ routes/api.php
@@ -249,8 +249,8 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
                     Route::prefix('type')->group(function () {
                         Route::put('all', [App\Http\Controllers\Api\Manager\NotificationController::class, 'updateTypeAll']);
                     });
-                    Route::put('type/{notification_type}', [App\Http\Controllers\Api\Manager\NotificationController::class, 'updateType']);
-
+                    Route::put('status', [App\Http\Controllers\Api\Manager\NotificationController::class, 'putStatus']);
+                    Route::put('status/all', [App\Http\Controllers\Api\Manager\NotificationController::class, 'putStatusAll']);
                     Route::prefix('{notification_id}')->group(function () {
                         Route::put('/', [App\Http\Controllers\Api\Manager\NotificationController::class, 'put']);
                         Route::delete('/', [App\Http\Controllers\Api\Manager\NotificationController::class, 'delete']);

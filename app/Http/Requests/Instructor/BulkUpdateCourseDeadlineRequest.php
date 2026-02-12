b5870450db8d6fb0853d03eb9c1af782c548d769
ryo maruyama <ryo@example.com>
2026-02-12 22:12:48 +0900

    不要な名前空間の削除

--- app/Http/Requests/Instructor/BulkUpdateCourseDeadlineRequest.php
+++ app/Http/Requests/Instructor/BulkUpdateCourseDeadlineRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests\Instructor;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
+
 
 class BulkUpdateCourseDeadlineRequest extends FormRequest
 {

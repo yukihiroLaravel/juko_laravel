588d3ac1a69358ec340af44a37d433c29104dd5b
daisuke nagafuchi <34060nagafuchidaisuke@gmail.com>
2026-02-23 12:31:05 +0900

    #1136 LessonControllerのdeleteメソッドの削除・動作確認4

--- app/Http/Controllers/Api/Manager/LessonController.php
+++ app/Http/Controllers/Api/Manager/LessonController.php
@@ -10,7 +10,6 @@ use App\Http\Requests\Manager\Lesson\UpdateTitleRequest;
 use App\Model\Lesson;
 use App\Model\LessonAttendance;
 use App\Services\Lesson\BulkDeleteLessonsService;
-use App\Services\Lesson\DeleteLessonService;
 use App\Services\Lesson\UpdateLessonService;
 use App\Services\Lesson\UpdateLessonTitleService;
 use Exception;

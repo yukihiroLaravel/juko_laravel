1516d4b321d2c74adb90c75dd8f285b42b630bea
mitsuru tamaki <tera0087@yahoo.co.jp>
2026-02-23 13:17:00 +0900

    JKA-1644空の配列を返すルーターとコントローラ実装

---
+++ app/Http/Controllers/Api/Student/LearningHistoryController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\Student;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LearningHistoryController extends Controller
+{
+    public function index() {
+        return response()->json([]);
+    }
+}

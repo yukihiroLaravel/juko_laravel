eadf3f7543d447186235699f49af76483e4b5ec0
Kou <hikkappi@yahoo.co.jp>
2026-02-27 20:53:28 +0900

    JKA-1664: 完了日時をimmutable_datetimeとしてAttendanceおよびLessonAttendanceモデルに追加

--- app/Model/Attendance.php
+++ app/Model/Attendance.php
@@ -115,6 +115,7 @@ class Attendance extends Model
      *   attendance_deadline: 'immutable_date',
      *   created_at: 'immutable_datetime',
      *   updated_at: 'immutable_datetime',
+     *   completed_at: 'immutable_datetime',
      * }
      */
     #[\Override]
--- app/Model/LessonAttendance.php
+++ app/Model/LessonAttendance.php
@@ -66,9 +66,9 @@ class LessonAttendance extends Model
     protected function casts(): array
     {
         return [
-            'created_at' => 'datetime',
-            'updated_at' => 'datetime',
-            'deleted_at' => 'datetime',
+            'created_at' => 'immutable_datetime',
+            'updated_at' => 'immutable_datetime',
+            'deleted_at' => 'immutable_datetime',
             'completed_at' => 'immutable_datetime',
         ];
     }

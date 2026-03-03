b5319fca991068e1151b35459ad978d26de026ad
Yoisana <your-email@example.com34060nagafuchidaisuke@gmail.com>
2026-03-03 22:51:44 +0900

    JKA-1666 講師側 講座更新APIで講座定員人数も更新できるように変更する(永渕)

--- app/Http/Resources/Base/Instructor/CourseResource.php
+++ app/Http/Resources/Base/Instructor/CourseResource.php
@@ -19,6 +19,7 @@ class CourseResource extends JsonResource
             'title' => $this->resource->title,
             'image' => $this->resource->image,
             'status' => $this->resource->status,
+            'capacity' => $this->resource->capacity,
             'deadline_type' => $this->resource->deadline_type,
             'course_deadline' => $this->resource->courseDeadline
                 ? new CourseDeadlineResource($this->resource->courseDeadline)

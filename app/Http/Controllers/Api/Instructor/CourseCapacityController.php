67068b5a6df7d9995b7f6e69091264ff88a0b2a9
Kou <hikkappi@yahoo.co.jp>
2026-03-27 21:28:20 +0900

    受講定員一括削除APIのリクエスト引数を修正し、認証方法をAuthからRequestに変更

--- app/Http/Controllers/Api/Instructor/CourseCapacityController.php
+++ app/Http/Controllers/Api/Instructor/CourseCapacityController.php
@@ -6,17 +6,17 @@ use App\Http\Controllers\Controller;
 use App\Model\Course;
 use App\Services\Instructor\CourseCapacity\ClearAllCapacityService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class CourseCapacityController extends Controller
 {
     /**
      * 受講定員一括削除API
      */
-    public function clearAll(ClearAllCapacityService $service): JsonResponse
+    public function clearAll(Request $request, ClearAllCapacityService $service): JsonResponse
     {
         // 自分の講座を取得
-        $courses = Course::where('instructor_id', Auth::id())->get();
+        $courses = Course::where('instructor_id', $request->user('instructor')->id)->get();
 
         // 認可
         $this->authorize('bulkUpdate', [Course::class, $courses]);

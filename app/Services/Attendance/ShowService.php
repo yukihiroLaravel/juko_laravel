51f09258dd8adf03046cd541b1ab0dcffb413838
deuro-ki <hero14yki1986@gmail.com>
2026-05-02 02:31:29 +0900

    feature/endo/JKA-1748/Logic_Implementation_To_Exclusion_Of_Student_Count(修正３回目)

--- app/Services/Attendance/ShowService.php
+++ app/Services/Attendance/ShowService.php
@@ -11,7 +11,7 @@ final class ShowService
      * 受講状況詳細に必要な情報（期限切れを除いた受講人数を含む）を取得する
      *
      * @return array{attendance: Attendance, studentsCount: int}
-     */
+    */
     public function __invoke(Attendance $attendance): array
     {
         $studentsCount = Attendance::where('course_id', $attendance->course_id)

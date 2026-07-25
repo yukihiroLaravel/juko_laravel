59d904faf0dfc6f02d42ac212014af13e175ff9e
daiki yaegashi <dashubazhongjian@gmail.com>
2026-07-25 16:32:26 +0900

    JKA-1767 受講生向け受講一覧APIの階層的可視性対応

--- app/Services/Student/Attendance/IndexService.php
+++ app/Services/Student/Attendance/IndexService.php
@@ -65,9 +65,9 @@ class IndexService
     private function getCompletedChaptersCount(Attendance $attendance): int
     {
         return $attendance->course->chapters->filter(fn (Chapter $chapter) =>
-            $chapter->status === ChapterStatusEnum::PUBLIC &&
+            $chapter->status === ChapterStatusEnum::PUBLIC->value &&
             $chapter->lessons->filter(
-                fn (Lesson $lesson) => $lesson->status === LessonStatusEnum::PUBLIC
+                fn (Lesson $lesson) => $lesson->status === LessonStatusEnum::PUBLIC->value
             )
             ->every(function (Lesson $lesson) use ($attendance) {
                 $lessonAttendance = $attendance->lessonAttendances->firstWhere('lesson_id', $lesson->id);
@@ -83,7 +83,7 @@ class IndexService
     {
         return $attendance->course->chapters
             ->filter(fn (Chapter $chapter) =>
-                    $chapter->status === ChapterStatusEnum::PUBLIC
+                    $chapter->status === ChapterStatusEnum::PUBLIC->value
             )
             ->count();
     }

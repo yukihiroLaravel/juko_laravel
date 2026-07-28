d107f255bf18123ad4a57ee6c34479e5618726b8
rie nishio <6nekohime@gmail.com>
2026-07-28 15:38:21 +0900

    JKA-1768 受講詳細APIの階層的可視性対応

--- app/Services/Student/Attendance/ShowService.php
+++ app/Services/Student/Attendance/ShowService.php
@@ -3,6 +3,8 @@
 namespace App\Services\Student\Attendance;
 
 use App\Dto\Student\Attendance\ShowDto;
+use App\Enums\Course\StatusEnum as CourseStatusEnum;
+use App\Enums\Lesson\StatusEnum as LessonStatusEnum;
 use App\Model\Attendance;
 use Illuminate\Auth\Access\AuthorizationException;
 
@@ -12,7 +14,7 @@ class ShowService
         ShowDto $showDto
     ): Attendance {
         $attendance = Attendance::with([
-            'course.publicChapters.lessons',
+            'course.publicChapters.lessons' => fn($q) => $q->where('status', LessonStatusEnum::PUBLIC->value),
             'course.instructor',
             'lessonAttendances',
             'course.courseDeadline',
@@ -21,9 +23,13 @@ class ShowService
 
         if ($attendance->student_id !== $showDto->getUserId()) {
             throw new AuthorizationException(
-                "The attendance's student ID {$attendance->student_id} ".
-                "does not match User ID {$showDto->getUserId()}."
+                "The attendance's student ID {$attendance->student_id} " .
+                    "does not match User ID {$showDto->getUserId()}."
             );
+        }
+
+        if ($attendance->course?->status?->value !== CourseStatusEnum::PUBLIC->value) {
+            throw new AuthorizationException('This course is not public.');
         }
 
         return $attendance;
